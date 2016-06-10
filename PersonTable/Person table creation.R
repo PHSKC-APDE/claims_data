d4026218d5e6786383d6d74d5f68813f84e2b470
alastairmatheson <alastair.matheson@gmail.com>
2016-06-10 15:42:02 -0700

    Created subfolders to organize code

---
+++ PersonTable/Person table creation.R
@@ -0,0 +1,12 @@
+###############################################################################
+# Code the create a cleaned person table from the Medicaid eligibility data
+# 
+# Alastair Matheson (PHSKC-APDE)
+# 2016-06-10
+###############################################################################
+
+
+library(RODBC)
+
+
+
