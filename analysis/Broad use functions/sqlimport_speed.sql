f1aeb49834a69a9c39a6ef5a594770f6fdce67c1
preason66 <ekern66@gmail.com>
2018-04-30 12:57:50 -0700

    nothing new

--- analysis/Broad use functions/sqlimport_speed.sql
+++ analysis/Broad use functions/sqlimport_speed.sql
@@ -2,3 +2,8 @@ BULK INSERT [PHClaims].[dbo].[temp]
 FROM '\\dchs-shares01\dchsdata\DCHSPHClaimsData\Data\temp.txt'
 WITH
 (FIELDTERMINATOR = ',', ROWTERMINATOR = '\\n')
+
+BULK INSERT [PHClaims].[dbo].[temp]
+FROM '\\PH3QGLK72\Temp\temp_fake.txt'
+WITH
+(FIELDTERMINATOR = ',', ROWTERMINATOR = '\\n')
