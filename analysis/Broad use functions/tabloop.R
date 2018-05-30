c7ffbd58438b89c7d051715ba06e7fdd033e58a2
preason66 <ekern66@gmail.com>
2018-05-30 11:56:16 -0700

    added feature to return zero counts for rows where permutation of fixed variables and loop variable does not exist

--- analysis/Broad use functions/tabloop.R
+++ analysis/Broad use functions/tabloop.R
@@ -3,11 +3,14 @@
 # 2018-5-14
 # APDE
 # Function to tabulate R data frame (e.g. summarize ndistinct) over fixed and looped by variables, binding all output as a single data frame
-# Version 1.0
+# Version 1.1
 
 #Example of running function:
 #tabloop_f(df = mydata, unit = id, loop = loop_var(age, race), fixed = fixed_var(year, region))
 #This would produce a single data frame using data in "mydata" tabulating distinct counts of "id" by year and region, separately by age and race
+
+##Version 1.1 update:
+#Modified function so that zero counts are returned for rows where permutation of fixed variables and loop variable do not exist in data
 ###############################################################################
 
 #### Define helper functions #####
@@ -37,11 +40,63 @@ tabloop_f <- function(df, unit, loop, fixed = NULL) {
     stop("Unit parameter has not been provided or is not valid column name in data frame; unit must be provided for tabulation")
   }
   
+  #### Step 1: Create matrix of fixed and loop by variables to allow padding for zero counts #### 
+  
+  #Create permutation matrix for fixed by variables
+  fix_matrix <- lapply(fixed, function(x) {
+    
+    #Process quosures
+    fix_matrix_var <-  enquo(x)
+    
+    #Distinct values of each by variable, create dummy linking variable
+    df <- df %>%
+      select(., !! fix_matrix_var) %>%
+      distinct(., !! fix_matrix_var) %>%
+      mutate(link = 1)
+    return(df)
+  }) %>%
+    
+    #Full join of resulting matrices for each fixed by variable
+    as.list(df) %>% 
+    Reduce(function(dtf1, dtf2) full_join(dtf1, dtf2, by = "link"), .)
+  
+  #Create stacked group_cat and group matrix for loop variables
+  loop_matrix <- lapply(loop, function(x) {
+    
+    loop_matrix_var <-  enquo(x)
+    
+    #Process names of loop variables
+    loop_var <- x 
+    group_name <- quo_name(loop_var)
+    group_name <- enquo(group_name)
+    
+    #Process data frame
+    df <- df %>%
+      select(., !! loop_matrix_var) %>%
+      distinct(., !! loop_matrix_var) %>%
+      mutate(link = 1,
+             group_cat = quo_name(loop_var)
+      ) %>%
+      rename(., group = !! group_name) %>%
+      mutate(
+        group = as.character(group)
+      )
+    return(df)
+  }) %>%
+    bind_rows() %>%
+    select(., group_cat, group, link)
+  
+  #Join fixed and loop var matrices
+  full_matrix <- full_join(fix_matrix, loop_matrix, by = "link") %>%
+    select(., -link)
+  
+  #### Step 2: Create results grouping by fixed and loop by variables #### 
+  
   #Process function arguments
   unit <- enquo(unit)
   
   #Tabulate data frame by fixed vars, looping over loop vars
-  lapply(loop, function(x) {
+  result <- lapply(loop, function(x) {
     
     #Process names of loop variables
     loop_var <- x 
@@ -70,8 +125,22 @@ tabloop_f <- function(df, unit, loop, fixed = NULL) {
     
     #Return one data frame for each loop variable provided
     return(df)
-    }) %>%
-    
+  }) %>%
     #Bind results of lapply function and return result
     bind_rows()
+  
+  #### Step 3: Join by variable matrix with tabulate results to add zero counts #### 
+  
+  #Process names of fixed by variables
+  fixed_name <- str_replace_all(as.character(fixed), "~", "")
+  merge_list <- c(fixed_name, "group_cat", "group")
+  
+  #Join
+  df <- left_join(full_matrix, result, by = merge_list) %>%
+    mutate(
+      count = case_when(
+        is.na(count) ~ as.integer(0),
+        TRUE ~ count
+      )
+    )
 }
