629f9119bb03bfc83260fc5eb053ab3c7a9bb6ca
preason66 <ekern66@gmail.com>
2018-07-26 13:19:34 -0700

    .gitignore refresh

--- analysis/Medicaid claims functions/archive/join speed.sql
+++
@@ -1,8 +0,0 @@
---this generates a float-type value
-select 150.0/181.0*100.0 as 'number'
-
---this generates a decimal-type with precision 3 and scale 1
-select cast((150 * 1.0) / (181 * 1.0) * (100.0) as decimal(3,1)) as 'number'
-
---try to use this information to control the precision of calculated columns in my SQL code, perhaps this
---will help speed of where clause in addition to indexing of columns
