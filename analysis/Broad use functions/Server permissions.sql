d28029a7029678e82776b82f9c190f116490cfd7
Kern <KERNELI@ph.lcl>
2019-01-16 13:57:58 -0800

    Delete Server permissions.sql

--- analysis/Broad use functions/Server permissions.sql
+++
@@ -1,8 +0,0 @@
-/****** Server permissions  ******/
-USE PH_APDEStore;
-SELECT * FROM fn_my_permissions(NULL, 'DATABASE');  
-GO
-
-USE PHClaims;
-SELECT * FROM fn_my_permissions(NULL, 'DATABASE');  
-GO
