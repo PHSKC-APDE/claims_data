d6dc5553367a0053049da0eddbb0f09926f846f4
Kern <KERNELI@ph.lcl>
2019-04-15 22:07:44 -0700

    APCD etl updates

---
+++ claims_db/phclaims/final/tables/load_final.apcd_elig_timevar.sql
@@ -0,0 +1,35 @@
+--Code to load data to final.apcd_elig_timevar table
+--Member characteristics for which we are primarily interested in variance over time. 
+--Includes program/eligibility coverage dates, residential address, and all geographic information (integer if possible). 
+--Member ID is not distinct, but member ID-from_date is distinct.
+--Eli Kern (PHSKC-APDE)
+--2019-4-12
+------------------
+--STEP 1: Insert data that has passed QA in stage schema table
+-------------------
+insert into PHClaims.final.apcd_elig_timevar with (tablock)
+select
+id_apcd,
+from_date,
+to_date,
+contiguous,
+med_covgrp,
+pharm_covgrp,
+med_medicaid,
+med_medicare,
+med_commercial,
+pharm_medicaid,
+pharm_medicare,
+pharm_commercial,
+dual,
+rac_code,
+geo_zip_code,
+geo_county_code,
+geo_county,
+geo_ach_code,
+geo_ach,
+cov_time_day
+from PHClaims.stage.apcd_elig_timevar;
+
+
+
---
+++ claims_db/phclaims/stage/tables/qa_stage.apcd_elig_timevar.sql
@@ -0,0 +1,164 @@
+--QA of stage.apcd_elig_demo table
+--4/11/19
+--Eli Kern
+
+----------------
+--INTERNAL CONSISTENCY: Aggregate member counts across tables
+----------------
+
+--All people using elig_timevar
+select count(distinct id_apcd) as id_dcount_timevar
+from phclaims.stage.apcd_elig_timevar;
+  
+--All people using member_month_detail
+select count(distinct internal_member_id) as id_dcount_mm
+from phclaims.stage.apcd_member_month_detail;
+  
+--All King people in 2016 using elig_covgrp
+select count(distinct id_apcd) as id_dcount_timevar
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+  and geo_ach = 'King';
+  
+--All King people in 2016 using member_month_detail
+select count(distinct internal_member_id) as id_dcount_mm
+from phclaims.stage.apcd_member_month_detail
+where left(year_month,4) = '2016'
+  and zip_code in (select zip_code from phclaims.ref.apcd_zip_group where zip_group_desc = 'King' and zip_group_type_desc = 'County');
+  
+--All King people in 2016 using eligibility
+select count(distinct internal_member_id) as id_dcount_elig
+from phclaims.stage.apcd_eligibility
+where eligibility_start_dt <= '2016-12-31' and eligibility_end_dt >= '2016-01-01'
+  and zip in (select zip_code from phclaims.ref.apcd_zip_group where zip_group_desc = 'King' and zip_group_type_desc = 'County');
+  
+--Investigate people who are in elig but not member_month
+--ANSWER: All of these people actually show up in the member_month_detail table and thus the source of the difference is ZIP code. When
+--a member has more than one ZIP code for a time period in eligibility table, OnPoint selects a single ZIP code. If this is not in King County then
+--this results in an apparent undercount.
+if object_id('tempdb..#temp1') is not null drop table #temp1;
+select b.internal_member_id
+into #temp1
+from (
+select distinct internal_member_id
+from phclaims.stage.apcd_member_month_detail
+where left(year_month,4) = '2016'
+  and zip_code in (select zip_code from phclaims.ref.apcd_zip_group where zip_group_desc = 'King' and zip_group_type_desc = 'County')
+) as a
+right join (
+select distinct internal_member_id
+from phclaims.stage.apcd_eligibility
+where eligibility_start_dt <= '2016-12-31' and eligibility_end_dt >= '2016-01-01'
+  and zip in (select zip_code from phclaims.ref.apcd_zip_group where zip_group_desc = 'King' and zip_group_type_desc = 'County')
+) as b
+on a.internal_member_id = b.internal_member_id
+where a.internal_member_id is null;
+
+select a.eligibility_id, a.submitter_id, a.internal_member_id, a.coverage_class, a.eligibility_start_dt, a.eligibility_end_dt, a.product_code, a.dual_eligibility_code, a.aid_category_id
+from phclaims.stage.apcd_eligibility as a
+inner join #temp1 as b
+on a.internal_member_id = b.internal_member_id
+order by a.internal_member_id, eligibility_start_dt
+
+select distinct a.internal_member_id, a.year_month, a.medical_eligibility_id, a.pharmacy_eligibility_id, a.zip_code
+from phclaims.stage.apcd_member_month_detail as a
+right join #temp1 as b
+on a.internal_member_id = b.internal_member_id
+where left(a.year_month,4) = '2016'
+order by a.internal_member_id, a.year_month
+
+
+----------------
+--INTERNAL CONSISTENCY: Member by member QA
+----------------
+--member with med_covgrp = 0: 12381222126
+--member with med_covgrp = 1: 11057531420
+--member with med_covgrp = 2: 12597524324
+--member with med_covgrp = 3: 11051242852
+--member with med_covgrp = 4: 12009284101, note it appears that reportable RAC is not being pulled, but rather all RAC codes
+--member with med_covgrp = 5: 11268451439
+--member with med_covgrp = 6: 11050760757
+--member with med_covgrp = 7: 11268450135
+--member with multiple ZIP codes and contiguous and non-contiguous rows: 11269028924
+
+select * from phclaims.stage.apcd_elig_timevar
+where id_apcd = 12381222126
+order by from_date;
+
+select * from phclaims.stage.apcd_member_month_detail
+where internal_member_id = 12381222126
+order by year_month;
+
+select eligibility_id, submitter_id, internal_member_id, coverage_class, eligibility_start_dt, eligibility_end_dt,
+  product_code, dual_eligibility_code_id, aid_category_code 
+from phclaims.stage.apcd_eligibility
+where internal_member_id = 12381222126
+--and coverage_class = 'MEDICAL'
+order by eligibility_start_dt;
+
+
+----------------
+--EXTERNAL CONSISTENCY: For comparison with ProviderOne data
+----------------
+--all people in King County in 2016
+select count(distinct id_apcd) as id_dcount
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+  and geo_ach = 'King';
+
+--all Medicaid members (medical) in King County in 2016
+--group by ID and find people with King residence and Medicaid coverage at any point in time in 2016
+select count(distinct id_apcd) as id_dcount
+from (
+select id_apcd, max(case when geo_ach = 'King' then 1 else 0 end) as king, max(med_medicaid) as med_medicaid
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+group by id_apcd
+) as a
+where king = 1 and med_medicaid = 1;
+  
+--all Medicaid members (medical or pharm) in King County in 2016
+--group by ID and find people with King residence and Medicaid coverage at any point in time in 2016
+select count(distinct id_apcd) as id_dcount
+from (
+select id_apcd, max(case when geo_ach = 'King' then 1 else 0 end) as king, max(med_medicaid) as med_medicaid, max(pharm_medicaid) as pharm_medicaid
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+group by id_apcd
+) as a
+where king = 1 and (med_medicaid = 1 or pharm_medicaid = 1);
+
+--all Medicaid members (medical and pharm) in King County in 2016
+--group by ID and find people with King residence and Medicaid coverage at any point in time in 2016
+select count(distinct id_apcd) as id_dcount
+from (
+select id_apcd, max(case when geo_ach = 'King' then 1 else 0 end) as king, max(med_medicaid) as med_medicaid, max(pharm_medicaid) as pharm_medicaid
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+group by id_apcd
+) as a
+where king = 1 and (med_medicaid = 1 and pharm_medicaid = 1);
+
+--all Medicaid-Medicare dual members (medical) in King County in 2016
+--group by ID and find people with King residence and Medicaid coverage at any point in time in 2016
+select count(distinct id_apcd) as id_dcount
+from (
+select id_apcd, max(case when geo_ach = 'King' then 1 else 0 end) as king, max(med_medicaid) as med_medicaid, max(pharm_medicaid) as pharm_medicaid,
+  max(med_medicare) as med_medicare, max(pharm_medicare) as pharm_medicare
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-01-01'
+group by id_apcd
+) as a
+where king = 1 and (med_medicaid = 1 and med_medicare = 1)
+
+--all Medicaid-Medicare dual members (medical or pharmacy) in King County in 2016
+--group by ID and find people with King residence and Medicaid coverage at any point in time in 2016
+select count(distinct id_apcd) as id_dcount
+from (
+select id_apcd, max(case when geo_ach = 'King' then 1 else 0 end) as king, max(med_medicaid) as med_medicaid, max(pharm_medicaid) as pharm_medicaid,
+  max(med_medicare) as med_medicare, max(pharm_medicare) as pharm_medicare
+from phclaims.stage.apcd_elig_timevar
+where from_date <= '2016-12-31' and to_date >= '2016-12-01'
+group by id_apcd
+) as a
+where king = 1 and ((med_medicaid = 1 or pharm_medicaid = 1) and (med_medicare = 1 or pharm_medicare = 1))
