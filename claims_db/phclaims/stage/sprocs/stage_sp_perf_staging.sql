be9dff39155cea38fddc03ed64c3433b334945d2
psylling <psylling@kingcounty.gov>
2019-05-07 13:04:34 -0700

    Added BHO treatment penetration functions

--- claims_db/phclaims/stage/sprocs/stage_sp_perf_staging.sql
+++ claims_db/phclaims/stage/sprocs/stage_sp_perf_staging.sql
@@ -622,3 +622,117 @@ EXEC sp_executeSQL @statement=@SQL,
                    @params=N'@start_month_int INT, @end_month_int INT, @measure_name VARCHAR(200)',
 				   @start_month_int=@start_month_int, @end_month_int=@end_month_int, @measure_name=@measure_name;
 GO
+
+
+
+USE [DCHS_Analytics];
+GO
+
+IF OBJECT_ID('[stage].[sp_perf_bho_staging]','P') IS NOT NULL
+DROP PROCEDURE [stage].[sp_perf_bho_staging];
+GO
+CREATE PROCEDURE [stage].[sp_perf_bho_staging]
+ @start_month_int INT = 201701
+,@end_month_int INT = 201712
+,@measure_name VARCHAR(200) = NULL
+AS
+SET NOCOUNT ON;
+DECLARE @SQL NVARCHAR(MAX) = '';
+DECLARE @start_month_date DATE;
+DECLARE @end_month_date DATE;
+
+BEGIN
+
+IF @measure_name = 'Mental Health Treatment Penetration'
+BEGIN
+
+DELETE FROM [stage].[perf_bho_staging]
+FROM [stage].[perf_bho_staging] AS a
+INNER JOIN [ref].[perf_measure] AS b
+ON a.[measure_id] = b.[measure_id]
+WHERE b.[measure_name] = @measure_name
+AND [year_month] >= @start_month_int
+AND [year_month] <= @end_month_int;
+
+SET @start_month_date = CAST(CAST(@start_month_int * 100 + 1 AS CHAR(8)) AS DATE);
+SET @end_month_date = EOMONTH(CAST(CAST(@end_month_int * 100 + 1 AS CHAR(8)) AS DATE));
+
+SET @SQL = @SQL + N'
+
+IF OBJECT_ID(''tempdb..#v_perf_bho_tpm_numerator'') IS NOT NULL
+DROP TABLE #v_perf_bho_tpm_numerator;
+SELECT *
+INTO #v_perf_bho_tpm_numerator
+FROM [stage].[v_perf_bho_tpm_numerator]
+WHERE [event_date] BETWEEN ''' + CAST(@start_month_date AS CHAR(10)) + ''' AND ''' + CAST(@end_month_date AS CHAR(10)) + ''';
+CREATE CLUSTERED INDEX idx_cl_#v_perf_bho_tpm_numerator ON #v_perf_bho_tpm_numerator([event_date]);
+
+IF OBJECT_ID(''tempdb..#v_perf_bho_tpm_denominator'') IS NOT NULL
+DROP TABLE #v_perf_bho_tpm_denominator;
+SELECT *
+INTO #v_perf_bho_tpm_denominator
+FROM [stage].[v_perf_bho_tpm_denominator]
+WHERE [event_date] BETWEEN ''' + CAST(@start_month_date AS CHAR(10)) + ''' AND ''' + CAST(@end_month_date AS CHAR(10)) + ''';
+CREATE CLUSTERED INDEX idx_cl_#v_perf_bho_tpm_denominator ON #v_perf_bho_tpm_denominator([event_date]);
+
+INSERT INTO [stage].[perf_bho_staging]
+([year_month]
+,[kcid]
+,[p1_id]
+,[measure_id]
+,[num_denom]
+,[measure_value]
+,[load_date])
+
+SELECT 
+ ym.[year_month]
+,a.[kcid]
+,c.[p1_id]
+,b.[measure_id]
+,''N'' AS [num_denom]
+,MAX(a.[flag]) AS [measure_value]
+,CAST(GETDATE() AS DATE) AS [load_date]
+
+FROM #v_perf_bho_tpm_numerator AS a
+INNER JOIN [ref].[perf_measure] AS b
+ON b.[measure_name] = ''' + @measure_name + '''
+INNER JOIN [ref].[perf_year_month] AS ym
+ON a.[event_date] BETWEEN ym.[beg_month] AND ym.[end_month]
+LEFT JOIN [php96].[client_id] AS c
+ON a.[kcid] = c.[kcid]
+GROUP BY ym.[year_month], a.[kcid], c.[p1_id], b.[measure_id];
+
+INSERT INTO [stage].[perf_bho_staging]
+([year_month]
+,[kcid]
+,[p1_id]
+,[measure_id]
+,[num_denom]
+,[measure_value]
+,[load_date])
+
+SELECT 
+ ym.[year_month]
+,a.[kcid]
+,c.[p1_id]
+,b.[measure_id]
+,''D'' AS [num_denom]
+,MAX(a.[flag]) AS [measure_value]
+,CAST(GETDATE() AS DATE) AS [load_date]
+
+FROM #v_perf_bho_tpm_denominator AS a
+INNER JOIN [ref].[perf_measure] AS b
+ON b.[measure_name] = ''' + @measure_name + '''
+INNER JOIN [ref].[perf_year_month] AS ym
+ON a.[event_date] BETWEEN ym.[beg_month] AND ym.[end_month]
+LEFT JOIN [php96].[client_id] AS c
+ON a.[kcid] = c.[kcid]
+GROUP BY ym.[year_month], a.[kcid], c.[p1_id], b.[measure_id];'
+END
+PRINT @SQL;
+END
+
+EXEC sp_executeSQL @statement=@SQL, 
+                   @params=N'@start_month_int INT, @end_month_int INT, @measure_name VARCHAR(200)',
+				   @start_month_int=@start_month_int, @end_month_int=@end_month_int, @measure_name=@measure_name;
+GO
