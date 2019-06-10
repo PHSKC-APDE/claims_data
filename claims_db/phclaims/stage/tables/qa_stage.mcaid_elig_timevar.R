a21f2522c134dfd64cac411747c073451c159419
Alastair Matheson <alastair.matheson@gmail.com>
2019-06-10 12:46:36 -0700

    Moved distinct ID check to run all the time

--- claims_db/phclaims/stage/tables/qa_stage.mcaid_elig_timevar.R
+++ claims_db/phclaims/stage/tables/qa_stage.mcaid_elig_timevar.R
@@ -2,26 +2,26 @@
 # Alastair Matheson
 # 2019-05
 
-# Code to QA stage.mcaid_elig_demo
+# Code to QA stage.mcaid_elig_timevar
 
 ###############################################################################
 
 
-qa_mcaid_elig_demo_f <- function(conn = db_claims,
-                                 load_only = F) {
+qa_mcaid_elig_timevar_f <- function(conn = db_claims,
+                                    load_only = F) {
   
-  # If this is the first time ever loading data, skip some checks.
+  # If this is the first time ever loading data, only load values.
   #   Otherwise, check against existing QA values
   
   #### PULL OUT VALUES NEEDED MULTIPLE TIMES ####
   # Rows in current table
   row_count <- as.numeric(odbc::dbGetQuery(conn, 
-                                           "SELECT COUNT (*) FROM stage.mcaid_elig_demo"))
+                                           "SELECT COUNT (*) FROM stage.mcaid_elig_timevar"))
   
   
   if (load_only == F) {
-    ### Pull out run date of stage.mcaid_elig_demo
-    last_run <- odbc::dbGetQuery(conn, "SELECT MAX (last_run) FROM stage.mcaid_elig_demo")
+    ### Pull out run date of stage.mcaid_elig_timevar
+    last_run <- odbc::dbGetQuery(conn, "SELECT MAX (last_run) FROM stage.mcaid_elig_timevar")
     
     
     #### COUNT NUMBER OF ROWS ####
@@ -30,12 +30,12 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
       odbc::dbGetQuery(conn, 
                        "SELECT a.* FROM
                        (SELECT * FROM metadata.qa_mcaid_values
-                         WHERE table_name = 'stage.mcaid_elig_demo' AND
+                         WHERE table_name = 'stage.mcaid_elig_timevar' AND
                           qa_item = 'row_count') a
                        INNER JOIN
                        (SELECT MAX(qa_date) AS max_date 
                          FROM metadata.qa_mcaid_values
-                         WHERE table_name = 'stage.mcaid_elig_demo' AND
+                         WHERE table_name = 'stage.mcaid_elig_timevar' AND
                           qa_item = 'row_count') b
                        ON a.qa_date = b.max_date"))
     
@@ -47,7 +47,7 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
         glue::glue_sql("INSERT INTO metadata.qa_mcaid
                    (last_run, table_name, qa_item, qa_result, qa_date, note) 
                    VALUES ({last_run}, 
-                   'stage.mcaid_elig_demo',
+                   'stage.mcaid_elig_timevar',
                    'Number new rows compared to most recent run', 
                    'FAIL', 
                    {Sys.time()}, 
@@ -63,7 +63,7 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
         glue::glue_sql("INSERT INTO metadata.qa_mcaid
                    (last_run, table_name, qa_item, qa_result, qa_date, note) 
                    VALUES ({last_run}, 
-                   'stage.mcaid_elig_demo',
+                   'stage.mcaid_elig_timevar',
                    'Number new rows compared to most recent run', 
                    'PASS', 
                    {Sys.time()}, 
@@ -72,13 +72,17 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
                        .con = conn))
       
     }
-    
   }
   
-  #### CHECK DISTINCT IDS = NUMBER OF ROWS ####
-  id_count <- as.numeric(odbc::dbGetQuery(conn, 
-                                          "SELECT COUNT (DISTINCT id_mcaid) 
-                                            FROM stage.mcaid_elig_demo"))
+  
+  #### CHECK DISTINCT IDS = DISTINCT IN STAGE ####
+  id_count_timevar <- as.numeric(odbc::dbGetQuery(conn, 
+                                                  "SELECT COUNT (DISTINCT id_mcaid) 
+                                                    FROM stage.mcaid_elig_timevar"))
+  
+  id_count_elig <- as.numeric(odbc::dbGetQuery(conn, 
+                                               "SELECT COUNT (DISTINCT id_mcaid) 
+                                                 FROM stage.mcaid_elig"))
   
   if (id_count != row_count) {
     odbc::dbGetQuery(
@@ -86,11 +90,11 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
       glue::glue_sql("INSERT INTO metadata.qa_mcaid
                        (last_run, table_name, qa_item, qa_result, qa_date, note) 
                        VALUES ({last_run}, 
-                       'stage.mcaid_elig_demo',
+                       'stage.mcaid_elig_timevar',
                        'Number distinct IDs', 
                        'FAIL', 
                        {Sys.time()}, 
-                       'There were {id_count} distinct IDs but {row_count} rows (should be the same)')",
+                       'There were {id_count_timevar} distinct IDs but {id_count_elig} in the raw data (should be the same)')",
                      .con = conn))
     
     stop(glue::glue("Number of distinct IDs doesn't match the number of rows. 
@@ -101,15 +105,14 @@ qa_mcaid_elig_demo_f <- function(conn = db_claims,
       glue::glue_sql("INSERT INTO metadata.qa_mcaid
                        (last_run, table_name, qa_item, qa_result, qa_date, note) 
                        VALUES ({last_run}, 
-                       'stage.mcaid_elig_demo',
+                       'stage.mcaid_elig_timevar',
                        'Number distinct IDs', 
                        'PASS', 
                        {Sys.time()}, 
-                       'The number of distinct IDs matched the number of rows ({id_count})')",
+                       'The number of distinct IDs matched number in raw data ({id_count_timevar})')",
                      .con = conn))
     
   }
-  
   
   
   #### LOAD VALUES TO QA_VALUES TABLE ####
