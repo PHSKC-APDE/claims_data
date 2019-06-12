aba98623c491d46c9cdcf3d0174fa23263a86abb
psylling <philip.sylling@gmail.com>
2019-06-12 10:39:51 -0700

    Update load_stage.mcaid_claim_icdcm_header.R

--- claims_db/phclaims/stage/tables/load_stage.mcaid_claim_icdcm_header.R
+++ claims_db/phclaims/stage/tables/load_stage.mcaid_claim_icdcm_header.R
@@ -1,25 +1,22 @@
 
-
-# This code creates table ([stage].[mcaid_claim_line]) to hold DISTINCT 
-# line-level claim information
+# This code creates table ([stage].[mcaid_claim_icdcm_header]) to hold DISTINCT 
+# procedure codes in long format for Medicaid claims data
 # 
-# Created by: Eli Kern, APDE, PHSKC, 2018-03-21
-# R functions created by Alastair Matheson, PHSKC (APDE), 2019-05
+# SQL script created by: Eli Kern, APDE, PHSKC, 2018-03-21
+# R functions created by: Alastair Matheson, PHSKC (APDE), 2019-05
 # Modified by: Philip Sylling, 2019-06-11
 # 
 # Data Pull Run time: XX min
 # Create Index Run Time: XX min
 # 
-# Table 'mcaid_claim'. Scan count 3, logical reads 8955218, physical reads 0, read-ahead reads 8926013, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-# Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 621302, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
-# 
 # Returns
-# [stage].[mcaid_claim_line]
+# [stage].[mcaid_claim_icdcm_header]
 #  [id_mcaid]
 # ,[claim_header_id]
-# ,[claim_line_id]
-# ,[rev_code]
-# ,[rac_code_line]
+# ,[icdcm_raw]
+# ,[icdcm_norm]
+# ,[icdcm_version]
+# ,[icdcm_number]
 # ,[last_run]
 
 #### Set up global parameter and call in libraries ####
@@ -42,45 +39,95 @@ library(tidyverse) # Manipulate data
 
 db_claims <- dbConnect(odbc(), "PHClaims")
 
-print("Creating stage.mcaid_claim_line")
+print("Creating stage.mcaid_claim_icdcm_header")
 
 #### SET UP FUNCTIONS ####
 devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/db_loader/scripts_general/create_table.R")
 
 step1_sql <- glue::glue_sql("
-if object_id('[stage].[mcaid_claim_line]', 'U') is not null
-drop table [stage].[mcaid_claim_line];
+if object_id('[stage].[mcaid_claim_icdcm_header]', 'U') is not null
+drop table [stage].[mcaid_claim_icdcm_header];
 ", .con = conn)
 odbc::dbGetQuery(conn = db_claims, step1_sql)
 
 #### CREATE TABLE ####
 create_table_f(conn = db_claims, 
-               config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/create_stage.mcaid_claim_line.yaml",
+               config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/create_stage.mcaid_claim_icdcm_header.yaml",
                overall = T, ind_yr = F)
 
 step2_sql <- glue::glue_sql("
-insert into [stage].[mcaid_claim_line] with (tablock)
-(id_mcaid
-,claim_header_id
-,claim_line_id
-,rev_code
-,rac_code_line
-,last_run)
+insert into [stage].[mcaid_claim_icdcm_header]
+([id_mcaid]
+,[claim_header_id]
+,[icdcm_raw]
+,[icdcm_norm]
+,[icdcm_version]
+,[icdcm_number]
+,[last_run])
 
-select 
-distinct
---top(100)
- MEDICAID_RECIPIENT_ID as id_mcaid
-,TCN as claim_header_id
-,CLM_LINE_TCN as claim_line_id
-,REVENUE_CODE as rev_code
-,RAC_CODE_L as rac_code_line
+select distinct
+ id_mcaid
+,claim_header_id
+--original diagnosis codes without zero right-padding
+,cast(diagnoses as varchar(200)) as icdcm_raw
+
+,	
+	cast(
+		case
+		    -- right-zero-pad ICD-9 diagnoses
+			when (diagnoses like '[0-9]%' and len(diagnoses) = 3) then diagnoses + '00'
+			when (diagnoses like '[0-9]%' and len(diagnoses) = 4) then diagnoses + '0'
+			-- Both ICD-9 and ICD-10 codes have 'V' and 'E' prefixes
+			-- Diagnoses prior to 2015-10-01 are ICD-9
+			when (diagnoses like 'V%' and TO_SRVC_DATE < '2015-10-01' and len(diagnoses) = 3) then diagnoses + '00'
+			when (diagnoses like 'V%' and TO_SRVC_DATE < '2015-10-01' and len(diagnoses) = 4) then diagnoses + '0'
+			when (diagnoses like 'E%' and TO_SRVC_DATE < '2015-10-01' and len(diagnoses) = 3) then diagnoses + '00'
+			when (diagnoses like 'E%' and TO_SRVC_DATE < '2015-10-01' and len(diagnoses) = 4) then diagnoses + '0'
+			else diagnoses 
+		end 
+	as varchar(200)) as icdcm_norm
+
+,
+	cast(
+		case
+			when (diagnoses like '[0-9]%') then 9
+			when (diagnoses like 'V%' and TO_SRVC_DATE < '2015-10-01') then 9
+			when (diagnoses like 'E%' and TO_SRVC_DATE < '2015-10-01') then 9
+			else 10 
+		end 
+	as tinyint) as icdcm_version
+
+,cast(dx_number as varchar(5)) as icdcm_number
 ,getdate() as last_run
 
-from [stage].[mcaid_claim];
+from 
+(
+select 
+ MEDICAID_RECIPIENT_ID as id_mcaid
+,TCN as claim_header_id
+--,CLM_LINE_TCN
+,TO_SRVC_DATE
+,PRIMARY_DIAGNOSIS_CODE as [01]
+,DIAGNOSIS_CODE_2 as [02]
+,DIAGNOSIS_CODE_3 as [03]
+,DIAGNOSIS_CODE_4 as [04]
+,DIAGNOSIS_CODE_5 as [05]
+,DIAGNOSIS_CODE_6 as [06]
+,DIAGNOSIS_CODE_7 as [07]
+,DIAGNOSIS_CODE_8 as [08]
+,DIAGNOSIS_CODE_9 as [09]
+,DIAGNOSIS_CODE_10 as [10]
+,DIAGNOSIS_CODE_11 as [11]
+,DIAGNOSIS_CODE_12 as [12]
+,ADMTNG_DIAGNOSIS_CODE as [admit]
+
+from stage.mcaid_claim
+) as a
+
+unpivot(diagnoses for dx_number IN ([01], [02], [03], [04], [05], [06], [07], [08], [09], [10], [11], [12], [admit])) as diagnoses;
 ", .con = conn)
 
-print("Running step 2: Load to [stage].[mcaid_claim_line]")
+print("Running step 2: Load to [stage].[mcaid_claim_icdcm_header]")
 time_start <- Sys.time()
 odbc::dbGetQuery(conn = db_claims, step2_sql)
 time_end <- Sys.time()
@@ -89,10 +136,10 @@ print(paste0("Step 2 took ", round(difftime(time_end, time_start, units = "secs"
              " mins)"))
 
 step3_sql <- glue::glue_sql("
-CREATE CLUSTERED INDEX [idx_cl_stage_mcaid_claim_line_claim_header_id] 
-ON [stage].[mcaid_claim_line]([claim_header_id]);
-CREATE NONCLUSTERED INDEX [idx_nc_stage_mcaid_claim_line_rev_code] 
-ON [stage].[mcaid_claim_line]([rev_code]);
+create clustered index [idx_cl_stage_mcaid_claim_icdcm_header_claim_header_id_icdcm_number]
+on [stage].[mcaid_claim_icdcm_header]([claim_header_id], [icdcm_number]);
+create nonclustered index [idx_nc_stage_mcaid_claim_icdcm_header_icdcm_version_icdcm_norm] 
+on [stage].[mcaid_claim_icdcm_header]([icdcm_version], [icdcm_norm]);
 ", .con = conn)
 
 print("Running step 3: Create Indexes")
