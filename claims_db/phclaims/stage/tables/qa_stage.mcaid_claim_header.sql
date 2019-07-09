4cb0acb5ae7aaf24810d7795f61941f407758da5
psylling <philip.sylling@gmail.com>
2019-07-09 12:11:14 -0700

    Update qa_stage.mcaid_claim_header.sql

--- claims_db/phclaims/stage/tables/qa_stage.mcaid_claim_header.sql
+++ claims_db/phclaims/stage/tables/qa_stage.mcaid_claim_header.sql
@@ -13,69 +13,203 @@ select count(distinct claim_header_id) as header_cnt
 from [stage].[mcaid_claim_header];
 select count(claim_header_id) as header_cnt
 from [stage].[mcaid_claim_header];
-select count(*) as header_cnt
-from [stage].[mcaid_claim_header];
-
---Confirm that claim header is distinct
---66,356,431
-select count(distinct claim_header_id) as header_cnt
-from [tmp].[mcaid_claim_header];
-select count(claim_header_id) as header_cnt
-from [tmp].[mcaid_claim_header];
-select count(*) as header_cnt
-from [tmp].[mcaid_claim_header];
 select count(distinct TCN) as header_cnt
 from [stage].[mcaid_claim];
+go
 
-select count(claim_header_id) as header_cnt
-from PHClaims.stage.apcd_claim_header;
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'Claim header distinct count check'
+,'PASS'
+,getdate()
+,'Claim header is distinct in mcaid_claim_header';
 
---Compare member counts
-select count(distinct id_apcd) as member_cnt
-from PHClaims.stage.apcd_claim_header;
+--All members should be in elig_demo and table
+select count(a.id_mcaid) as id_dcount
+from [stage].[mcaid_claim_header] as a
+where not exists
+(
+select 1 
+from [stage].[mcaid_elig_demo] as b
+where a.id_mcaid = b.id_mcaid
+);
+go
 
-select count(distinct internal_member_id) as member_cnt
-from #temp1
-where denied_line_min = 0 and orphaned_line_min = 0;
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'mcaid_elig_demo.id_mcaid check'
+,'PASS'
+,getdate()
+,'All members in mcaid_claim_header are in mcaid_elig_demo';
 
---All members should be in elig_demo and elig_timevar tables
-select count(a.id_apcd) as id_dcount
-from PHClaims.stage.apcd_claim_header as a
-left join PHClaims.final.apcd_elig_demo as b
-on a.id_apcd = b.id_apcd
-where b.id_apcd is null;
+--All members should be in elig_timevar table
+select count(a.id_mcaid) as id_dcount
+from [stage].[mcaid_claim_header] as a
+where not exists
+(
+select 1 
+from [final].[mcaid_elig_timevar] as b
+where a.id_mcaid = b.id_mcaid
+);
+go
 
-select count(a.id_apcd) as id_dcount
-from PHClaims.stage.apcd_claim_header as a
-left join PHClaims.final.apcd_elig_timevar as b
-on a.id_apcd = b.id_apcd
-where b.id_apcd is null;
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'mcaid_elig_time_var.id_mcaid check'
+,'PASS'
+,getdate()
+,'All members in mcaid_claim_header are in mcaid_elig_time_var';
 
---Compare claim header counts
-select count(distinct claim_header_id) as header_cnt
-from PHClaims.stage.apcd_claim_header;
+-- Compare number of claim headers in current vs. prior analytic tables
+WITH [final] AS
+(
+SELECT
+ YEAR([from_date]) AS [claim_year]
+,COUNT([tcn]) AS [prior_claim_header]
+FROM [PHClaims].[dbo].[mcaid_claim_summary] AS a
+GROUP BY YEAR([from_date])
+),
 
-select count(distinct medical_claim_header_id) as header_cnt
-from #temp1
-where denied_line_min = 0 and orphaned_line_min = 0;
+[stage] AS
+(
+SELECT
+ YEAR([first_service_date]) AS [claim_year]
+,COUNT([claim_header_id]) AS [current_claim_header]
+FROM [stage].[mcaid_claim_header] AS a
+GROUP BY YEAR([first_service_date])
+)
 
---Compare sum of provider ID
-select sum(provider_id_apcd) as provider_id_sum
-from PHClaims.stage.apcd_claim_header;
+SELECT
+ COALESCE(a.[claim_year], b.[claim_year]) AS [claim_year]
+,[prior_claim_header]
+,[current_claim_header]
+,CAST([current_claim_header] AS NUMERIC) / [prior_claim_header] AS [pct_change]
+FROM [final] AS a
+FULL JOIN [stage] AS b
+ON a.[claim_year] = b.[claim_year]
+ORDER BY [claim_year];
+GO
 
-select sum(cast(internal_provider_id as bigint)) as provider_id_sum
-from #temp1
-where denied_line_min = 0 and orphaned_line_min = 0;
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'Compare new-to-prior claim headers'
+,'PASS'
+,getdate()
+,'Ratio 2%-4% more claim headers per year';
 
---Compare claim header counts by ICD-CM version
-select icdcm_version, count(claim_header_id) as header_cnt
-from PHClaims.stage.apcd_claim_header
-group by icdcm_version;
+-- Compare number of ed visits in current vs. prior analytic tables
+WITH [final] AS
+(
+SELECT
+ YEAR([from_date]) AS [claim_year]
+,SUM([ed]) AS [prior_ed]
+FROM [PHClaims].[dbo].[mcaid_claim_summary] AS a
+GROUP BY YEAR([from_date])
+),
 
-select icd_version_ind, count(medical_claim_header_id) as header_cnt
-from #temp1
-where denied_line_min = 0 and orphaned_line_min = 0
-group by icd_version_ind;
+[stage] AS
+(
+SELECT
+ YEAR([first_service_date]) AS [claim_year]
+,SUM([ed]) AS [current_ed]
+FROM [stage].[mcaid_claim_header] AS a
+GROUP BY YEAR([first_service_date])
+)
+
+SELECT
+ COALESCE(a.[claim_year], b.[claim_year]) AS [claim_year]
+,[prior_ed]
+,[current_ed]
+,CAST([current_ed] AS NUMERIC) / [prior_ed] AS [pct_change]
+FROM [final] AS a
+FULL JOIN [stage] AS b
+ON a.[claim_year] = b.[claim_year]
+ORDER BY [claim_year];
+GO
+
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'Compare new-to-prior ed visits'
+,'PASS'
+,getdate()
+,'Very close - except 2017 ED visits dropped 3.5% in new extract';
+
+-- Compare number of inpatient stays in current vs. prior analytic tables
+WITH [final] AS
+(
+SELECT
+ YEAR([from_date]) AS [claim_year]
+,SUM([inpatient]) AS [prior_inpatient]
+FROM [PHClaims].[dbo].[mcaid_claim_summary] AS a
+GROUP BY YEAR([from_date])
+),
+
+[stage] AS
+(
+SELECT
+ YEAR([first_service_date]) AS [claim_year]
+,SUM([inpatient]) AS [current_inpatient]
+FROM [stage].[mcaid_claim_header] AS a
+GROUP BY YEAR([first_service_date])
+)
+
+SELECT
+ COALESCE(a.[claim_year], b.[claim_year]) AS [claim_year]
+,[prior_inpatient]
+,[current_inpatient]
+,CAST([current_inpatient] AS NUMERIC) / [prior_inpatient] AS [pct_change]
+FROM [final] AS a
+FULL JOIN [stage] AS b
+ON a.[claim_year] = b.[claim_year]
+ORDER BY [claim_year];
+GO
+
+declare @last_run as datetime = (select max(last_run) from [stage].[mcaid_claim_header]);
+insert into [metadata].[qa_mcaid]
+select 
+ NULL
+,@last_run
+,'stage.mcaid_claim_header'
+,'Compare new-to-prior extract inpatient stays'
+,'PASS'
+,getdate()
+,'Generally 5% higher - except 2017 inpatient stays only 1% higher';
+
+SELECT [etl_batch_id]
+      ,[last_run]
+      ,[table_name]
+      ,[qa_item]
+      ,[qa_result]
+      ,[qa_date]
+      ,[note]
+FROM [PHClaims].[metadata].[qa_mcaid]
+WHERE [table_name] = 'stage.mcaid_claim_header';
+
+
+
+
+
+
 
 --Compare ED visit counts
 select sum(ed_flag) as ed_cnt
