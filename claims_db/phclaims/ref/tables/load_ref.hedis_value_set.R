53b0c56fe73ebbad182fb6cb99bf77b86ac9b85d
psylling <philip.sylling@gmail.com>
2019-07-09 09:26:24 -0700

    Update load_ref.hedis_value_set.R

--- claims_db/phclaims/ref/tables/load_ref.hedis_value_set.R
+++ claims_db/phclaims/ref/tables/load_ref.hedis_value_set.R
@@ -57,11 +57,32 @@ input <- read.xlsx(paste0(file.dir, "M. HEDIS 2019 Volume 2 VSD 11_05_2018.xlsx"
 tbl <- Id(schema="tmp", table="HEDIS_2019_Volume_2_VSD_11_05_2018-5.xlsx")
 dbWriteTable(db.connection, name=tbl, value=input, overwrite=TRUE)
 
-# HEDIS Medications
-file.dir <- "L:/DCHSPHClaimsData/References/HEDIS/2018/"
+library("dplyr")
+library("dbplyr")
+library("DBI")
+library("odbc")
+library("tidyr")
+library("glue")
+library("devtools")
+#library("medicaid")
+library("openxlsx")
+library("lubridate")
+library("janitor")
 
-input <- read.xlsx(paste0(file.dir, "20180208_HEDIS_NDC_MLD_CompleteDirectory_Workbook_2018.xlsx"), sheet = 2)
-dbWriteTable(db.connection, "tmp_20180208_HEDIS_NDC_MLD_CompleteDirectory_Workbook_2018-2.xlsx", input, overwrite=TRUE)
+dsn <- "PHClaims"
+db.connection <- dbConnect(odbc(), dsn)
 
-input <- read.xlsx(paste0(file.dir, "20180208_HEDIS_NDC_MLD_CompleteDirectory_Workbook_2018.xlsx"), sheet = 3)
-dbWriteTable(db.connection, "tmp_20180208_HEDIS_NDC_MLD_CompleteDirectory_Workbook_2018-3.xlsx", input, overwrite=TRUE)
+# HEDIS Medications 2019
+file.dir <- "L:/DCHSPHClaimsData/HEDIS/2019/Medications List Directory/"
+
+input <- read.xlsx(paste0(file.dir, "HEDIS-2019-NDC-MLD-Directory-Complete-Workbook-FINAL-11-1-2018-2.xlsx"), sheet = 2)
+tbl <- Id(schema="tmp", table="HEDIS_2019_NDC_MLD_Directory-2.xlsx")
+dbWriteTable(db.connection, name=tbl, value=input, overwrite=TRUE)
+
+input <- read.xlsx(paste0(file.dir, "HEDIS-2019-NDC-MLD-Directory-Complete-Workbook-FINAL-11-1-2018-2.xlsx"), sheet = 3)
+tbl <- Id(schema="tmp", table="HEDIS_2019_NDC_MLD_Directory-3.xlsx")
+dbWriteTable(db.connection, name=tbl, value=input, overwrite=TRUE)
+
+input <- read.xlsx(paste0(file.dir, "HEDIS-2019-NDC-MLD-Directory-Complete-Workbook-FINAL-11-1-2018-2.xlsx"), sheet = 4)
+tbl <- Id(schema="tmp", table="HEDIS_2019_NDC_MLD_Directory-4.xlsx")
+dbWriteTable(db.connection, name=tbl, value=input, overwrite=TRUE)
