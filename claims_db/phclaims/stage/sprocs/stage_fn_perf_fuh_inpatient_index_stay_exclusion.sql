6f40223d0ccbef56c730a647bb1ad398b40efc3f
psylling <philip.sylling@gmail.com>
2019-08-09 15:48:10 -0700

    Update P4P Code

--- claims_db/phclaims/stage/sprocs/stage_fn_perf_fuh_inpatient_index_stay_exclusion.sql
+++ claims_db/phclaims/stage/sprocs/stage_fn_perf_fuh_inpatient_index_stay_exclusion.sql
@@ -12,19 +12,20 @@ LOGIC:
 denotes a valid inpatient index stay
 
 Author: Philip Sylling
-Last Modified: 2019-04-25
+Created: 2019-04-25
+Modified: 2019-08-09 | Point to new [final] analytic tables
 
 Returns:
- [id]
+ [id_mcaid]
 ,[age]
-,[tcn]
-,[from_date]
-,[to_date]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag], = 1
 ,[inpatient_within_30_day], = 0 if no subsequent readmission
 */
 
-USE PHClaims;
+USE [PHClaims];
 GO
 
 IF OBJECT_ID('[stage].[fn_perf_fuh_inpatient_index_stay_exclusion]', 'TF') IS NOT NULL
@@ -38,94 +39,94 @@ CREATE FUNCTION [stage].[fn_perf_fuh_inpatient_index_stay_exclusion]
 ,@exclusion_value_set_name VARCHAR(100))
 
 RETURNS @inpatient_index_stay_exclusion TABLE
-([id] VARCHAR(200) NULL 
+([id_mcaid] VARCHAR(255) NULL 
 ,[age] INT NULL
-,[tcn] VARCHAR(200) NULL
-,[from_date] DATE NULL
-,[to_date] DATE NULL
-,[flag] INT NULL
+,[claim_header_id] BIGINT NULL
+,[first_service_date] DATE NULL
+,[last_service_date] DATE NULL
+,[flag] INT NOT NULL
 ,[inpatient_within_30_day] INT NULL)
  AS
 BEGIN
 
 DECLARE @index_stay TABLE
-([id] VARCHAR(200) NULL 
+([id_mcaid] VARCHAR(255) NULL 
 ,[age] INT NULL
-,[tcn] VARCHAR(200) NULL
-,[from_date] DATE NULL
-,[to_date] DATE NULL
-,[flag] INT NULL
-,INDEX idx_cl_index_stay_id_from_date CLUSTERED([id], [from_date]));
+,[claim_header_id] BIGINT NULL
+,[first_service_date] DATE NULL
+,[last_service_date] DATE NULL
+,[flag] INT NOT NULL
+,INDEX idx_cl_index_stay_id_mcaid_first_service_date CLUSTERED([id_mcaid], [first_service_date]));
 
 INSERT INTO @index_stay
-([id]
+([id_mcaid]
 ,[age]
-,[tcn]
-,[from_date]
-,[to_date]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag])
 
 SELECT 
- [id]
+ [id_mcaid]
 ,[age]
-,[tcn]
-,[from_date]
-,[to_date]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag]
 FROM [stage].[fn_perf_fuh_inpatient_index_stay]
 (@measurement_start_date, @measurement_end_date, @age, @dx_value_set_name);
 
 DECLARE @readmit TABLE
-([id] VARCHAR(200) NULL 
-,[tcn] VARCHAR(200) NULL
-,[from_date] DATE NULL
-,[to_date] DATE NULL
-,[flag] INT NULL
-,INDEX idx_cl_readmit_id_from_date CLUSTERED([id], [from_date]));
+([id_mcaid] VARCHAR(255) NULL 
+,[claim_header_id] BIGINT NULL
+,[first_service_date] DATE NULL
+,[last_service_date] DATE NULL
+,[flag] INT NOT NULL
+,INDEX idx_cl_readmit_id_mcaid_first_service_date CLUSTERED([id_mcaid], [first_service_date]));
 
 INSERT INTO @readmit
-([id]
-,[tcn]
-,[from_date]
-,[to_date]
+([id_mcaid]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag])
 
 SELECT 
- [id]
-,[tcn]
-,[from_date]
-,[to_date]
+ [id_mcaid]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag]
 FROM [stage].[fn_perf_fuh_inpatient_index_stay_readmit]
 (@measurement_start_date, @measurement_end_date, @exclusion_value_set_name);
 
 INSERT INTO @inpatient_index_stay_exclusion
-([id]
+([id_mcaid]
 ,[age]
-,[tcn]
-,[from_date]
-,[to_date]
+,[claim_header_id]
+,[first_service_date]
+,[last_service_date]
 ,[flag]
 ,[inpatient_within_30_day])
 
 SELECT 
- a.[id]
+ a.[id_mcaid]
 ,a.[age]
-,a.[tcn]
-,a.[from_date]
-,a.[to_date]
+,a.[claim_header_id]
+,a.[first_service_date]
+,a.[last_service_date]
 ,a.[flag]
 ,MAX(ISNULL(b.[flag], 0)) AS [inpatient_within_30_day]
 FROM @index_stay AS a
 LEFT JOIN @readmit AS b
-ON a.[id] = b.[id]
-AND b.[from_date] BETWEEN a.[to_date] AND DATEADD(DAY, 30, a.[to_date])
+ON a.[id_mcaid] = b.[id_mcaid]
+AND b.[first_service_date] BETWEEN a.[last_service_date] AND DATEADD(DAY, 30, a.[last_service_date])
 GROUP BY 
- a.[id]
+ a.[id_mcaid]
 ,a.[age]
-,a.[tcn]
-,a.[from_date]
-,a.[to_date]
+,a.[claim_header_id]
+,a.[first_service_date]
+,a.[last_service_date]
 ,a.[flag];
 
 RETURN  
