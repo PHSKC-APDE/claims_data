be242d90af0a9ae9592777f83d04a831e45a7dbc
Danny Colombara <dcolombara@kingcounty.gov>
2019-08-14 14:00:32 -0700

    Create initial empty Mcare QA tables ... based on Mcaid QA tables

---
+++ claims_db/phclaims/metadata/tables/create_metadata.qa_mcare.R
@@ -0,0 +1,23 @@
+# Author: Danny Colombara
+# 
+# Date: August 14, 2019
+#
+# R version: 3.5.3
+#
+# Purpose: Create SQL Medicare QA tables (just a shell that will be filled with QA process data)
+#          Follow Medicaid tables as a template 
+#
+
+## Set up environment ----
+    pacman::p_load(odbc, glue)
+
+## Connect to SQL server ----
+    db_apde <- dbConnect(odbc(), "PHClaims51") 
+
+## Create [PHClaims].[metadata].[qa_mcaid] ----
+    setDT(DBI::dbGetQuery(db_apde, "SELECT TOP 0 * INTO metadata.qa_mcare FROM metadata.qa_mcaid"))
+    
+## Create [PHClaims].[metadata].[qa_mcaid_values] ----
+    setDT(DBI::dbGetQuery(db_apde, "SELECT TOP 0 * INTO metadata.qa_mcare_values FROM metadata.qa_mcaid_values"))  
+    
+## The end! 
