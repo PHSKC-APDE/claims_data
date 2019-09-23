3ae4d43e9b023098da91feaaa203acb866c58199
Danny Colombara <dcolombara@kingcounty.gov>
2019-09-23 11:17:33 -0700

    SQL code to copy QA'd [stage] files to the [final] schema

---
+++ claims_db/phclaims/final/tables/load_final.mcaid_mcare_elig_demo.sql
@@ -0,0 +1,28 @@
+/****** COPY FROM STAGE to FINAL  ******/
+	IF OBJECT_ID('[PHClaims].[final].[mcaid_mcare_elig_demo]', 'U') IS NOT NULL 
+		DROP TABLE [PHClaims].[final].[mcaid_mcare_elig_demo]
+
+	SELECT *
+		INTO [PHClaims].[final].[mcaid_mcare_elig_demo]	
+		FROM [PHClaims].[stage].[mcaid_mcare_elig_demo]
+
+
+/****** ADD COLUMSTORE CLUSTERED INDEX ******/
+	CREATE CLUSTERED COLUMNSTORE INDEX idx_final_mcaid_mcare_elig_demo
+	ON [PHClaims].[final].[mcaid_mcare_elig_demo]
+	WITH (DROP_EXISTING = OFF)
+
+
+/****** BASIC ERROR CHECKING COMPARING STAGE & FINAL ******/
+	SELECT COUNT(*) FROM [PHClaims].[stage].[mcaid_mcare_elig_demo]
+	SELECT COUNT(*) FROM [PHClaims].[final].[mcaid_mcare_elig_demo]
+
+	SELECT geo_kc, 
+	count(*) FROM [PHClaims].[stage].[mcaid_mcare_elig_demo]
+	  GROUP BY geo_kc
+	  ORDER BY -count(*)
+
+	 SELECT geo_kc, 
+	count(*) FROM [PHClaims].[final].[mcaid_mcare_elig_demo]
+	  GROUP BY geo_kc
+	  ORDER BY -count(*)
