c61bf08e92db32b5251ccc36a782c4e8f2539981
psylling <philip.sylling@gmail.com>
2019-09-25 14:07:10 -0700

    Update FUH

--- claims_db/phclaims/stage/sprocs/create_stage.sp_perf_fuh_join_step.R
+++ claims_db/phclaims/stage/sprocs/create_stage.sp_perf_fuh_join_step.R
@@ -301,12 +301,87 @@ FROM [stage].[fn_perf_fuh_inpatient_index_stay_readmit]('2017-01-01', '2018-12-3
 ")
 
 direct_transfer <- inner_join(index_stay_mental_illness, index_stay_mental_health_diagnosis, c("flag")) %>%
-  mutate(date_diff=difftime(admit_date.y, discharge_date.x, units="days"))
+  mutate(date_diff=difftime(admit_date.y, discharge_date.x, units="days")) %>%
+  filter(id_mcaid.x==id_mcaid.y & date_diff>=1 & date_diff<=30) %>%
+  select(data_source=data_source.x, value_set_name=value_set_name.x, id_mcaid=id_mcaid.x, age=age.x, admit_date=admit_date.x, discharge_date=discharge_date.x, data_source.y, value_set_name.y, id_mcaid.y, age.y, admit_date.y, discharge_date.y) 
 
-  filter(id_mcaid.x==id_mcaid.y & admit_date.y>discharge_date.x )
-difftime(time1, time2, tz,
-         units = c("auto", "secs", "mins", "hours",
-                   "days", "weeks"))
+index_stay_mental_illness_2 <- left_join(index_stay_mental_illness, direct_transfer, c("data_source", "value_set_name", "id_mcaid", "age", "admit_date", "discharge_date")) %>%
+  group_by(data_source, value_set_name, id_mcaid, age, admit_date, discharge_date, flag) %>%
+  summarize(max_age=max(age.y), max_discharge_date=max(discharge_date.y)) %>%
+  mutate(new_age=ifelse(is.na(max_age), age, max_age)) %>%
+  mutate(new_discharge_date=as.Date(ifelse(is.na(max_discharge_date), discharge_date, max_discharge_date), origin="1970-01-01")) %>% ungroup() %>%
+  select(data_source, value_set_name, id_mcaid, age=new_age, admit_date, discharge_date=new_discharge_date, flag)
+
+readmission <- inner_join(index_stay_mental_illness_2, readmit, c("id_mcaid")) %>%
+  mutate(date_diff=difftime(admit_date.y, discharge_date.x, units="days")) %>%
+  filter(date_diff>=0 & date_diff<=30) %>%
+  select(data_source=data_source.y, id_mcaid, admit_date=admit_date.y, discharge_date=discharge_date.y, acuity) %>%
+  group_by(data_source, id_mcaid, admit_date, discharge_date, acuity) %>% summarize(flag=1)
+
+index_stay_mental_illness_3 <- left_join(index_stay_mental_illness_2, readmission, c("data_source", "id_mcaid")) %>%
+  mutate(date_diff=difftime(admit_date.y, discharge_date.x, units="days")) %>%
+  mutate(inpatient_within_30_day=ifelse(date_diff>=1 & date_diff<=30, 1, 0)) %>%
+  mutate(inpatient_within_30_day=ifelse(is.na(inpatient_within_30_day), 0, inpatient_within_30_day)) %>%
+  group_by(data_source, value_set_name, id_mcaid, age, admit_date.x, discharge_date.x, flag.x) %>%
+  summarize(max_inpatient_within_30_day=max(inpatient_within_30_day)) %>% ungroup() %>%
+  mutate(year_month=year(discharge_date.x) * 100 + month(discharge_date.x)) %>%
+  mutate(need_1_month_coverage=ifelse(day(discharge_date.x)==1, 1, 0)) %>%
+  select(year_month, id_mcaid, age, admit_date=admit_date.x, discharge_date=discharge_date.x, inpatient_index_stay=flag.x, inpatient_within_30_day=max_inpatient_within_30_day, need_1_month_coverage) %>%
+  filter(inpatient_within_30_day==0)
+
+fuh_follow_up_visit_mcaid <- dbGetQuery(PHClaims, "
+SELECT 
+ id_mcaid
+,service_date
+,flag
+,only_30_day_fu
+FROM [stage].[v_perf_fuh_follow_up_visit]
+WHERE [service_date] BETWEEN '2017-01-01' AND '2018-12-31';
+")
+
+fuh_follow_up_visit_bho <- dbGetQuery(DCHS_Analytics, "
+SELECT 
+ p1_id AS id_mcaid
+,event_date AS service_date
+,flag
+,only_30_day_fu
+FROM [stage].[fn_perf_fuh_follow_up_visit]('2017-01-01', '2018-12-31')
+WHERE [p1_id] IS NOT NULL;
+")
+
+follow_up_7_day <- rbind(fuh_follow_up_visit_mcaid, fuh_follow_up_visit_bho) %>%
+  filter(only_30_day_fu=="N")
+follow_up_7_day <- left_join(index_stay_mental_illness_3, follow_up_7_day, c("id_mcaid")) %>%
+  mutate(date_diff=difftime(service_date, discharge_date, units="days")) %>%
+  filter(date_diff>=1 & date_diff<=7) %>%
+  group_by(year_month, id_mcaid, age, admit_date, discharge_date, inpatient_index_stay, inpatient_within_30_day, need_1_month_coverage, service_date) %>%
+  summarize(flag=1) %>% ungroup()
+
+follow_up_30_day <- rbind(fuh_follow_up_visit_mcaid, fuh_follow_up_visit_bho)
+follow_up_30_day <- left_join(index_stay_mental_illness_3, follow_up_30_day, c("id_mcaid")) %>%
+  mutate(date_diff=difftime(service_date, discharge_date, units="days")) %>%
+  filter(date_diff>=1 & date_diff<=30) %>%
+  group_by(year_month, id_mcaid, age, admit_date, discharge_date, inpatient_index_stay, inpatient_within_30_day, need_1_month_coverage, service_date) %>%
+  summarize(flag=1) %>% ungroup()
+
+temp_7_day <- left_join(index_stay_mental_illness_3, follow_up_7_day, c("year_month", "id_mcaid", "age", "admit_date", "discharge_date", "inpatient_index_stay", "inpatient_within_30_day", "need_1_month_coverage")) %>%
+  mutate(flag=ifelse(is.na(flag), 0 , flag)) %>%
+  group_by(year_month, id_mcaid, age, admit_date, discharge_date, inpatient_index_stay, inpatient_within_30_day, need_1_month_coverage) %>%
+  summarize(follow_up_7_day=max(flag)) %>% ungroup()
+
+temp_30_day <- left_join(index_stay_mental_illness_3, follow_up_30_day, c("year_month", "id_mcaid", "age", "admit_date", "discharge_date", "inpatient_index_stay", "inpatient_within_30_day", "need_1_month_coverage")) %>%
+  mutate(flag=ifelse(is.na(flag), 0 , flag)) %>%
+  group_by(year_month, id_mcaid, age, admit_date, discharge_date, inpatient_index_stay, inpatient_within_30_day, need_1_month_coverage) %>%
+  summarize(follow_up_30_day=max(flag)) %>% ungroup()
+
+stat_7 <- temp_7_day %>%
+  filter(discharge_date>="2017-10-01" & discharge_date<="2018-08-30") %>%
+  summarize(inpatient_index_stay=sum(inpatient_index_stay), follow_up_7_day=sum(follow_up_7_day))
+stat_30 <- temp_30_day %>%
+  filter(discharge_date>="2017-10-01" & discharge_date<="2018-08-30") %>%
+  summarize(inpatient_index_stay=sum(inpatient_index_stay), follow_up_30_day=sum(follow_up_30_day))
+
+
 
 # Output results #
 # today <- Sys.Date()
