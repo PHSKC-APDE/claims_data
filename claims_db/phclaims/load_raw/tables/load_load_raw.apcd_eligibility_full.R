64053e9e067b5af269be697d3841e1546e692e63
Eli Kern <eli.kern@kingcounty.gov>
2019-10-04 15:33:14 -0700

    Fixing params

--- claims_db/phclaims/load_raw/tables/load_load_raw.apcd_eligibility_full.R
+++ claims_db/phclaims/load_raw/tables/load_load_raw.apcd_eligibility_full.R
@@ -48,7 +48,7 @@ load_load_raw.apcd_eligibility_full_f <- function(etl_date_min = NULL,
   print("Loading tables to SQL")
   load_table_from_file_f(conn = db_claims,
                          config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.apcd_eligibility_full.yaml",
-                         overall = T, ind_yr = F, combine_yr = F)
+                         overall = F, ind_yr = T, combine_yr = T)
   
   
   #### ADD BATCH ID COLUMN ####
