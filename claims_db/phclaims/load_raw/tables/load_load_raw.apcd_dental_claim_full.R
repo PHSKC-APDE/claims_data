ae0b9fac5461f349af614291d419c41e928c051c
Eli Kern <eli.kern@kingcounty.gov>
2019-10-17 09:08:23 -0700

    Fixing config url

--- claims_db/phclaims/load_raw/tables/load_load_raw.apcd_dental_claim_full.R
+++ claims_db/phclaims/load_raw/tables/load_load_raw.apcd_dental_claim_full.R
@@ -50,8 +50,8 @@ load_load_raw.apcd_dental_claim_full_f <- function(etl_date_min = NULL,
   #### LOAD TABLES ####
   print("Loading tables to SQL")
   load_table_from_file_f(conn = db_claims,
-                         config_url = glue("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.",
-                                           table_name_part, "full.yaml"),
+                         config_url = paste0("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.",
+                                           table_name_part, "_full.yaml"),
                          overall = F, ind_yr = T, combine_yr = T, test_mode = F)
   
   
@@ -67,7 +67,7 @@ load_load_raw.apcd_dental_claim_full_f <- function(etl_date_min = NULL,
   
   
   #### LOAD YAML CONFIG FILE FOR FINAL COMMANDS ####
-  config_url <- glue("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.", table_name_part, "full.yaml")
+  config_url <- paste0("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.", table_name_part, "_full.yaml")
   if (!is.null(config_url)) {
     table_config <- yaml::yaml.load(RCurl::getURL(config_url))
   } else {
