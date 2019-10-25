1b79787dec7afce9e87679fe9fce9bc95cac6d30
Eli Kern <eli.kern@kingcounty.gov>
2019-10-24 22:32:39 -0700

    Trying global temp tables instead of local

--- claims_db/phclaims/stage/tables/load_stage.apcd_elig_timevar.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_elig_timevar.R
@@ -32,15 +32,15 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
     -------------------
     --STEP 1: Join distinct member IDs with year-month matrix
     -------------------
-    if object_id('tempdb..#id') is not null drop table #id;
+    if object_id('tempdb..##id') is not null drop table ##id;
     select distinct internal_member_id, 1 as flag
-    into #id
+    into ##id
     from phclaims.stage.apcd_member_month_detail;
     
-    if object_id('tempdb..#id_month') is not null drop table #id_month;
+    if object_id('tempdb..##id_month') is not null drop table ##id_month;
     select a.internal_member_id, b.first_day_month, b.last_day_month
-    into #id_month
-    from #id as a
+    into ##id_month
+    from ##id as a
     full join (select first_day_month, last_day_month, 1 as flag from phclaims.ref.date where first_day_month >= '2014-01-01' and last_day_month <= {extract_end_date}) as b
     on a.flag = b.flag;
     
@@ -48,10 +48,10 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
     -------------------
     --STEP 2: Join with eligibility table by member ID
     -------------------
-    if object_id('tempdb..#temp1') is not null drop table #temp1;
+    if object_id('tempdb..##temp1') is not null drop table ##temp1;
     select a.internal_member_id, a.first_day_month, a.last_day_month, b.eligibility_start_dt, b.eligibility_end_dt, b.dual_flag, b.rac_code_id
-    into #temp1
-    from #id_month as a
+    into ##temp1
+    from ##id_month as a
     left join (
       select internal_member_id, eligibility_start_dt,
       --set ongoing eligibility end dates to latest extract end date 
@@ -79,14 +79,14 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
     --are not desginated primary, we have to choose one
     --Thus, take max of full_benefit flag, and then take max of BSP code (thus higher numbered BSPs chosen)
     -------------------
-    if object_id('tempdb..#temp2') is not null drop table #temp2;
+    if object_id('tempdb..##temp2') is not null drop table ##temp2;
     select x.internal_member_id, x.first_day_month, x.last_day_month, max(x.dual_flag) as dual_flag,
     	max(x.bsp_group_cid) as bsp_group_cid, max(x.full_benefit) as full_benefit
-    into #temp2
+    into ##temp2
     from (
     select a.internal_member_id, a.first_day_month, a.last_day_month, a.dual_flag, c.bsp_group_cid,
     	case when c.full_benefit = 'Y' then 1 else 0 end as full_benefit
-    from #temp1 as a
+    from ##temp1 as a
     left join PHClaims.ref.apcd_aid_category as b
     on a.rac_code_id = b.aid_category_id
     left join PHClaims.ref.mcaid_rac_code as c
@@ -98,7 +98,7 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
     -------------------
     --STEP 4: Join eligibility-based dual and RAC info to member_month_detail table and create coverage group variables
     -------------------
-    if object_id('tempdb..#temp3') is not null drop table #temp3;
+    if object_id('tempdb..##temp3') is not null drop table ##temp3;
     select a.internal_member_id, a.first_day_month as from_date,
       dateadd(day, -1, dateadd(month, 1, a.first_day_month)) as to_date,
       a.zip_code, b.dual_flag, b.bsp_group_cid, b.full_benefit,
@@ -125,34 +125,34 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
         when (a.rx_medicaid_eligibility_id is not null or b.bsp_group_cid is not null) and a.rx_commercial_eligibility_id is not null and (a.rx_medicare_eligibility_id is not null or b.dual_flag = 1) then 7 -- All three
         else 0 --no pharm coverage
        end as pharm_covgrp
-    into #temp3
+    into ##temp3
     from (
     select *, convert(date, cast(year_month as varchar(200)) + '01') as first_day_month
     from phclaims.stage.apcd_member_month_detail
     ) as a
-    left join #temp2 as b
+    left join ##temp2 as b
     on a.internal_member_id = b.internal_member_id and a.first_day_month = b.first_day_month;
     
     
     ------------
     --STEP 5: Assign a group number to each set of contiguous months by person, covgrp, dual_flag, BSP code, and full benefit flag, and ZIP code
     ----------------
-    if object_id('tempdb..#temp4') is not null drop table #temp4;
+    if object_id('tempdb..##temp4') is not null drop table ##temp4;
     select distinct internal_member_id, from_date, to_date, zip_code, med_covgrp, pharm_covgrp, dual_flag, bsp_group_cid, full_benefit,
     	datediff(month, '1900-01-01', from_date) - row_number() 
     	over (partition by internal_member_id, zip_code, med_covgrp, pharm_covgrp, dual_flag, bsp_group_cid, full_benefit order by from_date) as group_num
-    into #temp4
-    from #temp3;
+    into ##temp4
+    from ##temp3;
     
     
     ------------
     --STEP 6: Taking the max and min of each contiguous period, collapse to one row
     ----------------
-    if object_id('tempdb..#temp5') is not null drop table #temp5;
+    if object_id('tempdb..##temp5') is not null drop table ##temp5;
     select internal_member_id, zip_code, med_covgrp, pharm_covgrp, dual_flag, bsp_group_cid, full_benefit, min(from_date) as from_date, max(to_date) as to_date,
       datediff(day, min(from_date), max(to_date)) + 1 as cov_time_day
-    into #temp5
-    from #temp4
+    into ##temp5
+    from ##temp4
     group by internal_member_id, zip_code, med_covgrp, pharm_covgrp, dual_flag, bsp_group_cid, full_benefit, group_num;
     
     
@@ -187,7 +187,7 @@ load_stage.apcd_elig_timevar_f <- function(extract_end_date = NULL) {
     case when b.zip_group_code is not null then 1 else 0 end as geo_wa,
     a.cov_time_day,
     getdate() as last_run
-    from #temp5 as a
+    from ##temp5 as a
     left join (select zip_code, zip_group_code, zip_group_desc from phclaims.ref.apcd_zip_group where zip_group_type_desc = 'County') as b
     on a.zip_code = b.zip_code
     left join (select zip_code, zip_group_code, zip_group_desc from phclaims.ref.apcd_zip_group where left(zip_group_type_desc, 3) = 'Acc') as c
