cfd784dfe4fe964e759a4cafa0964cd46d5b65a2
Eli Kern <eli.kern@kingcounty.gov>
2019-10-28 08:59:18 -0700

    Update master_apcd_analytic.R

--- claims_db/db_loader/apcd/master_apcd_analytic.R
+++ claims_db/db_loader/apcd/master_apcd_analytic.R
@@ -29,38 +29,69 @@ devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/m
 devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/db_loader/scripts_general/claim_ccw.R")
 
 ## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
-#### STEP 1: Load and QA data on stage schema ####
+#### Table 1: apcd_elig_demo ####
 ## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
 
-#### apcd_elig_demo ####
-### Create table
+### A) Call in functions
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_demo.R")
+
+### B) Create table
 create_table_f(conn = db_claims, 
                config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_demo.yaml",
-               overall = T,
-               ind_yr = F,
-               overwrite = T,
-               test_mode = F)
+               overall = T, ind_yr = F, overwrite = T, test_mode = F)
 
-### Load tables
-# Call in function
-#Have to replace below script with final script (which will include load and qa functions)
-#devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.apcd_dental_claim_full.R")
+### C) Load tables
 system.time(load_stage.apcd_elig_demo_f())
 
-### Testing space for QA code (eventually append as qa_stage.apcd_elig_demo_f() and add to above sourced script)
-res1 <- dbGetQuery(conn = db_claims, glue_sql(
-  "select 'stage.apcd_elig_demo' as 'table', 'distinct count' as qa_type, count(distinct id_apcd) as qa from stage.apcd_elig_demo",
-  .con = db_claims))
-res2 <- dbGetQuery(conn = db_claims, glue_sql(
-  "select 'stage.apcd_member_month_detail' as 'table', 'distinct count' as qa_type, count(distinct internal_member_id) as qa from stage.apcd_member_month_detail",
-  .con = db_claims))
-res3 <- dbGetQuery(conn = db_claims, glue_sql(
-  "select 'stage.apcd_elig_demo' as 'table', 'count' as qa_type, count(id_apcd) as qa from stage.apcd_elig_demo",
-  .con = db_claims))
-apcd_demo_qa1 <- bind_rows(res1, res2, res3)
-rm(list = ls(pattern = "^res"))
+### D) Table-level QA
+system.time(apcd_demo_qa1 <- qa_stage.apcd_elig_demo_f())
+rm(apcd_demo_qa1)
 
-#Reminder - run line-level QA script at \\dchs-shares01\dchsdata\dchsphclaimsdata\qa_line_level\qa_stage.apcd_elig_demo.sql             
+### E) Run line-level QA script at \\dchs-shares01\dchsdata\dchsphclaimsdata\qa_line_level\qa_stage.apcd_elig_demo.sql             
+
+### F) Archive current table
+alter_schema_f(conn = db_claims, from_schema = "final", to_schema = "archive", table_name = "apcd_elig_demo")
+
+### G) Alter schema on new table
+alter_schema_f(conn = db_claims, from_schema = "stage", to_schema = "final", table_name = "apcd_elig_demo")
+
+### H) Create clustered columnstore index
+system.time(dbSendQuery(conn = db_claims, glue_sql("create clustered columnstore index idx_ccs_final_apcd_elig_demo on final.apcd_elig_demo")))
 
 
+## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
+#### Table 2: ref.apcd_id_year_month_matrix ####
+# Modify script later to only add new IDs as this table takes a while to run
+## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
 
+### A) Call in functions
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_year_month_matrix.R")
+
+### B) Create table
+create_table_f(conn = db_claims, 
+               config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_year_month_matrix.yaml",
+               overall = T, ind_yr = F, overwrite = T, test_mode = F)
+
+### C) Load tables
+system.time(load_stage.apcd_elig_year_month_matrix_full_f(extract_end_date = "2019-03-31"))
+#Placeholder for partial refresh function
+
+### D) Table-level QA
+system.time(apcd_elig_matrix_qa1 <- qa_stage.apcd_elig_year_month_matrix_f())
+rm(apcd_elig_matrix_qa1)
+
+
+## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
+#### Table 3: apcd_elig_timevar ####
+## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
+
+### A) Call in functions
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_timevar.R")
+
+### B) Create table
+create_table_f(conn = db_claims, 
+               config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.apcd_elig_timevar.yaml",
+               overall = T, ind_yr = F, overwrite = T, test_mode = F)
+
+### C) Load tables
+system.time(load_stage.apcd_elig_timevar_f(extract_end_date = "2019-03-31"))
