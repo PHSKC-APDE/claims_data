7a23cf5ac0e1917ab1d7e7792ad7fa7e75a2e287
Danny Colombara <dcolombara@kingcounty.gov>
2019-10-31 13:18:17 -0700

    Ensure id_mcare maintains case sensitivity & drop stage when final live

--- claims_db/phclaims/final/tables/load_final.xwalk_apde_mcaid_mcare_pha.sql
+++ claims_db/phclaims/final/tables/load_final.xwalk_apde_mcaid_mcare_pha.sql
@@ -7,6 +7,11 @@
 		FROM [PHClaims].[stage].[xwalk_apde_mcaid_mcare_pha]
 
 
+/****** Ensure case sensitivity for Mcare id ******/
+ALTER TABLE [PHClaims].[final].[xwalk_apde_mcaid_mcare_pha]
+ALTER COLUMN id_mcare varchar(255) collate SQL_Latin1_General_CP1_CS_AS NULL;
+
+
 /****** ADD COLUMSTORE CLUSTERED INDEX ******/
 	CREATE CLUSTERED COLUMNSTORE INDEX idx_final_xwalk_apde_mcaid_mcare_pha
 	ON [PHClaims].[final].[xwalk_apde_mcaid_mcare_pha]
@@ -19,3 +24,6 @@
 
 	SELECT SUM(CAST(id_apde AS BIGINT)) FROM [PHClaims].[stage].[xwalk_apde_mcaid_mcare_pha]
 	SELECT SUM(CAST(id_apde AS BIGINT)) FROM [PHClaims].[final].[xwalk_apde_mcaid_mcare_pha]
+
+/****** DROP STAGE TABLE ******/
+	DROP TABLE [PHClaims].[stage].[xwalk_apde_mcaid_mcare_pha]
