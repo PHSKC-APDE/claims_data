97da71d4f2153fb114fbfe3ba9a64862efbdc489
psylling <philip.sylling@gmail.com>
2019-11-12 15:01:01 -0800

    Update Treatment Penetration by Dx

--- claims_db/phclaims/stage/tables/load_stage.mcaid_claim_MASTER.R
+++ claims_db/phclaims/stage/tables/load_stage.mcaid_claim_MASTER.R
@@ -15,6 +15,9 @@ library(stringr)
 library(tidyr)
 library(tidyverse) # Manipulate data
 
+#
+# These scripts depend only on [stage].[mcaid_claim]
+#
 print("Creating stage.mcaid_claim_icdcm_header")
 source("C:/Users/psylling/github/claims_data/claims_db/phclaims/stage/tables/load_stage.mcaid_claim_icdcm_header.R")
 print("Creating stage.mcaid_claim_line")
@@ -23,3 +26,9 @@ print("Creating stage.mcaid_claim_pharm")
 source("C:/Users/psylling/github/claims_data/claims_db/phclaims/stage/tables/load_stage.mcaid_claim_pharm.R")
 print("Creating stage.mcaid_claim_procedure")
 source("C:/Users/psylling/github/claims_data/claims_db/phclaims/stage/tables/load_stage.mcaid_claim_procedure.R")
+
+#
+# Building the stage.mcaid_claim_header table depends on the analytic tables created above
+#
+print("Creating stage.mcaid_claim_header")
+source("C:/Users/psylling/github/claims_data/claims_db/phclaims/stage/tables/load_stage.mcaid_claim_header.R")
