d54686edc0122c86b42e040b1e1170b893f30eb5
Eli Kern <eli.kern@kingcounty.gov>
2020-01-17 09:48:33 -0800

    Added code to exclude no elig claims

--- claims_db/phclaims/stage/tables/archive/load_stage.mcare_claim_line.sql
+++ claims_db/phclaims/stage/tables/archive/load_stage.mcare_claim_line.sql
@@ -14,8 +14,8 @@ insert into PHClaims.stage.mcare_claim_line with (tablock)
 --bcarrier
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -26,15 +26,19 @@ getdate() as last_run
 from PHClaims.stage.mcare_bcarrier_line as a
 left join PHClaims.stage.mcare_bcarrier_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
 where b.denial_code in ('1','2','3','4','5','6','7','8','9')
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null
 
 --dme
 union
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -45,8 +49,12 @@ getdate() as last_run
 from PHClaims.stage.mcare_dme_line as a
 left join PHClaims.stage.mcare_dme_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
 where b.denial_code in ('1','2','3','4','5','6','7','8','9')
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null
 
 --hha
 --placeholder once we receive HHA revenue center tables
@@ -55,8 +63,8 @@ where b.denial_code in ('1','2','3','4','5','6','7','8','9')
 union
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -67,15 +75,19 @@ getdate() as last_run
 from PHClaims.stage.mcare_hospice_revenue_center as a
 left join PHClaims.stage.mcare_hospice_base_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
 where b.denial_code_facility = '' or b.denial_code_facility is null
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null
 
 --inpatient
 union
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -86,15 +98,19 @@ getdate() as last_run
 from PHClaims.stage.mcare_inpatient_revenue_center as a
 left join PHClaims.stage.mcare_inpatient_base_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
 where b.denial_code_facility = '' or b.denial_code_facility is null
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null
 
 --outpatient
 union
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -105,15 +121,19 @@ getdate() as last_run
 from PHClaims.stage.mcare_outpatient_revenue_center as a
 left join PHClaims.stage.mcare_outpatient_base_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
 where b.denial_code_facility = '' or b.denial_code_facility is null
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null
 
 --snf
 union
 select
 top 100
-a.id_mcare,
-a.claim_header_id,
+rtrim(a.id_mcare) as id_mcare,
+rtrim(a.claim_header_id) as claim_header_id,
 a.claim_line_id,
 b.first_service_date,
 b.last_service_date,
@@ -124,7 +144,9 @@ getdate() as last_run
 from PHClaims.stage.mcare_snf_revenue_center as a
 left join PHClaims.stage.mcare_snf_base_claims as b
 on a.claim_header_id = b.claim_header_id
+left join PHClaims.final.mcare_elig_demo as c
+on a.id_mcare = c.id_mcare
 --exclude denined claims using carrier/dme claim method
-where b.denial_code_facility = '' or b.denial_code_facility is null;
-
-
+where b.denial_code_facility = '' or b.denial_code_facility is null
+--exclude claims among people who have eligibility data
+and c.id_mcare is not null;
