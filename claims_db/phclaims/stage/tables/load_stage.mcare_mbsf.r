0b6a3a7e9cd8476740095bbe5abd6309734bf928
Danny Colombara <dcolombara@kingcounty.gov>
2020-01-21 16:24:54 -0800

    Updated load_stage.mcare_mbsf.r for 2017
     - Generalized code so that it will automatically add a single
       year of new MBSF AB or MBSF ABCD data that appears in
       load_raw, but not in stage.
     - Entire process updated and confirmed working as expected.

--- claims_db/phclaims/stage/tables/load_stage.mcare_mbsf.r
+++ claims_db/phclaims/stage/tables/load_stage.mcare_mbsf.r
@@ -15,7 +15,7 @@
 
 ## Set up environment ----
     rm(list=ls())
-    .libPaths("C:/Users/dcolombara/R.packages") # needed for 32 GB SAS computer.
+    # .libPaths("C:/Users/dcolombara/R.packages") # needed for 32 GB SAS computer.
     pacman::p_load(data.table, dplyr, odbc, lubridate, glue, httr)
     start.time <- Sys.time()
     options("scipen"=999) # turn off scientific notation  
@@ -23,84 +23,111 @@
     setwd("C:/temp/")
     yaml.url <- "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.mcare_mbsf.yaml"
 
-## (1) Connect to SQL Server ----    
+## Connect to the servers ----
+    sql_server = "KCITSQLUTPDBH51"
+    sql_server_odbc_name = "PHClaims51"
+    db.claims51 <- dbConnect(odbc(), sql_server_odbc_name) ##Connect to SQL server    
+    
+## (1) Connect to SQL Server & get YAML data ----    
     db_claims <- dbConnect(odbc(), "PHClaims51") 
 
-## (2) PULL & PREP MBSF AB ----
-    # count rows in load_raw 
-    ab.raw.count <- as.numeric(odbc::dbGetQuery(db_claims, 
-                                             "SELECT COUNT (*) FROM load_raw.mcare_mbsf_ab"))
-    # pull load_raw into R
-    ab <- (
-      odbc::dbGetQuery(db_claims, 
-                       "SELECT DISTINCT * FROM load_raw.mcare_mbsf_ab"))
-    setDT(ab)
-    
-    # ensure there are no errors when pulling data from SQL
-    if(ab.raw.count != nrow(ab))
-      stop("Mismatching row count, error reading in data")
-    
-    # read in YAML data for new stage file from GitHub
     table_config <- yaml::yaml.load(RCurl::getURL(yaml.url))
     
-    # rename vars to match those in MBSF ABCD
-    setnames(ab, names(table_config$rename_ab_abcd), unlist(table_config$rename_ab_abcd))
-    
-    # fix zip codes by adding preceding zeros and setting to character 
-    ab[zip_cd=="999999999", zip_cd := NA]
-    ab[, zip_cd := substr(zip_cd, 1, 5)]    
-    
-    # identify the column types needed for stage file in SQL
-    ab.stage.cols <- data.table(col.name = names(table_config$vars), col.type = table_config$vars)[col.name %in% names(ab)]
-    ab.characters <- ab.stage.cols[tolower(col.type) %like% "char", ]$col.name
-    ab.integers <- ab.stage.cols[tolower(col.type) %like% "int", ]$col.name
-    ab.dates <- ab.stage.cols[tolower(col.type) %like% "date", ]$col.name
-    rm(ab.stage.cols)
+## (2) Identify data that does not already exist in MBSF Stage ----
+    # get tables of all counts by year
+      count.stage <- data.table::setDT(dbGetQuery(conn = db.claims51, "SELECT bene_enrollmt_ref_yr, count(bene_enrollmt_ref_yr) FROM [PHClaims].[stage].[mcare_mbsf] GROUP BY bene_enrollmt_ref_yr"))[, stage := TRUE]
+      count.raw.ab <- data.table::setDT(dbGetQuery(conn = db.claims51, "SELECT bene_enrollmt_ref_yr, count(bene_enrollmt_ref_yr) FROM [PHClaims].[load_raw].[mcare_mbsf_ab] GROUP BY bene_enrollmt_ref_yr"))[, mbsf := "ab"]
+      count.raw.abcd <- data.table::setDT(dbGetQuery(conn = db.claims51, "SELECT bene_enrollmt_ref_yr, count(bene_enrollmt_ref_yr) FROM [PHClaims].[load_raw].[mcare_mbsf_abcd] GROUP BY bene_enrollmt_ref_yr"))[, mbsf := "abcd"]
 
-    # change the class of the R columns if necessary
-    ab[, (ab.characters):= lapply(.SD, as.character), .SDcols = ab.characters]
-    ab[, (ab.integers):= lapply(.SD, as.integer), .SDcols = ab.integers]
-    ab[, (ab.dates):= lapply(.SD, as.Date), .SDcols = ab.dates]
-    
-## (3) PULL & prep MBSF ABCD ---- 
-    # count rows in load_raw 
-    abcd.raw.count <- as.numeric(odbc::dbGetQuery(db_claims, 
-                                             "SELECT COUNT (*) FROM load_raw.mcare_mbsf_abcd"))
-    # pull load_raw into R
-    abcd <- (
-      odbc::dbGetQuery(db_claims, 
-                       "SELECT DISTINCT * FROM load_raw.mcare_mbsf_abcd"))
-    setDT(abcd)
-    
-    # drop 'data_year' because redundant with bene_enrollmt_ref_yr
-    abcd[, data_year := NULL]
+      lapply(list(count.stage, count.raw.ab, count.raw.abcd), setnames, old = 2, new = "count")
+      
+    # user merge to identify rows that need to be added to stage
+      count.raw.ab <- merge(count.stage, count.raw.ab, by = c("bene_enrollmt_ref_yr", "count"), all.y = T)[is.na(stage)]
+      count.raw.abcd <- merge(count.stage, count.raw.abcd, by = c("bene_enrollmt_ref_yr", "count"), all.y = T)[is.na(stage)]
 
-    # ensure there are no errors when pulling data from SQL
-    if(abcd.raw.count != nrow(abcd))
-      stop("Mismatching row count, error reading in data")
-    
-    # fix zip codes by adding preceding zeros and setting to character 
-    abcd[zip_cd=="99999", zip_cd := NA]    
-    abcd[, zip_cd := formatC(as.numeric(zip_cd), width = 5, format = "d", flag="0")]
-    
-    # identify the column types needed for stage file in SQL
-    abcd.stage.cols <- data.table(col.name = names(table_config$vars), col.type = table_config$vars)[col.name %in% names(abcd)]
-    abcd.characters <- abcd.stage.cols[tolower(col.type) %like% "char", ]$col.name
-    abcd.integers <- abcd.stage.cols[tolower(col.type) %like% "int", ]$col.name
-    abcd.dates <- abcd.stage.cols[tolower(col.type) %like% "date", ]$col.name
-    rm(abcd.stage.cols)
+## (3) PULL & PREP the new year of MBSF AB (if needed) ----
+        add.year <- unique(count.raw.ab$bene_enrollmt_ref_yr)
+          
+        if(nrow(count.raw.ab)){
+            # count rows in load_raw 
+              ab.raw.count <- as.numeric(dbGetQuery(conn = db.claims51, glue::glue("SELECT count(*) FROM [load_raw].[mcare_mbsf_ab] where bene_enrollmt_ref_yr = {add.year}", .con = db.claims51)))
+              
+            # pull load_raw into R
+              ab <- data.table::setDT((dbGetQuery(conn = db.claims51, glue::glue("SELECT DISTINCT * FROM [load_raw].[mcare_mbsf_ab] where bene_enrollmt_ref_yr = {add.year}", .con = db.claims51))) )
+            
+            # ensure there are no errors when pulling data from SQL
+              if(ab.raw.count != nrow(ab)){stop("Mismatching row count, error reading in data")}
+              
+        } else{
+          ab.raw.count <- 0
+          ab <- data.table::setDT(dbGetQuery(conn = db.claims51, "SELECT top(0) * FROM [load_raw].[mcare_mbsf_ab]"))
+        }
+          
+        # rename vars to match those in MBSF ABCD
+          setnames(ab, names(table_config$rename_ab_abcd), unlist(table_config$rename_ab_abcd))
+          
+        # fix zip codes by adding preceding zeros and setting to character 
+          ab[zip_cd=="999999999", zip_cd := NA]
+          ab[, zip_cd := substr(zip_cd, 1, 5)]    
+          
+        # identify the column types needed for stage file in SQL
+          ab.stage.cols <- data.table(col.name = names(table_config$vars), col.type = table_config$vars)[col.name %in% names(ab)]
+          ab.characters <- ab.stage.cols[tolower(col.type) %like% "char", ]$col.name
+          ab.integers <- ab.stage.cols[tolower(col.type) %like% "int", ]$col.name
+          ab.dates <- ab.stage.cols[tolower(col.type) %like% "date", ]$col.name
+          rm(ab.stage.cols)
+          
+        # change the class of the R columns if necessary
+          ab[, (ab.characters):= lapply(.SD, as.character), .SDcols = ab.characters]
+          ab[, (ab.integers):= lapply(.SD, as.integer), .SDcols = ab.integers]
+          ab[, (ab.dates):= lapply(.SD, as.Date), .SDcols = ab.dates]
 
-    # change the class of the R columns if necessary
-    abcd[, (abcd.characters):= lapply(.SD, as.character), .SDcols = abcd.characters]
-    abcd[, (abcd.integers):= lapply(.SD, as.integer), .SDcols = abcd.integers]
-    abcd[, (abcd.dates):= lapply(.SD, as.Date), .SDcols = abcd.dates]
 
-## (4) Append MBSF AB & ABCD ----    
+## (4) PULL & prep the new year of MBSF ABCD (if needed) ---- 
+      add.year <- unique(count.raw.abcd$bene_enrollmt_ref_yr)
+          
+      if(nrow(count.raw.abcd)){
+          
+      # count rows in load_raw 
+        abcd.raw.count <- as.numeric(dbGetQuery(conn = db.claims51, glue::glue("SELECT count(*) FROM [load_raw].[mcare_mbsf_abcd] where bene_enrollmt_ref_yr = {add.year}", .con = db.claims51)))
+        
+      # pull load_raw into R
+        abcd <- data.table::setDT((dbGetQuery(conn = db.claims51, glue::glue("SELECT DISTINCT * FROM [load_raw].[mcare_mbsf_abcd] where bene_enrollmt_ref_yr = {add.year}", .con = db.claims51))) )
+
+      # drop 'data_year' because redundant with bene_enrollmt_ref_yr
+        abcd[, data_year := NULL]
+    
+      # ensure there are no errors when pulling data from SQL
+        if(abcd.raw.count != nrow(abcd)){stop("Mismatching row count, error reading in data")}
+        
+      } else {
+        abcd.raw.count <- 0
+        abcd <- data.table::setDT(dbGetQuery(conn = db.claims51, "SELECT top(0) * FROM [load_raw].[mcare_mbsf_abcd]")) 
+      }        
+        
+      # fix zip codes by adding preceding zeros and setting to character 
+        abcd[zip_cd=="99999", zip_cd := NA]    
+        abcd[, zip_cd := formatC(as.numeric(zip_cd), width = 5, format = "d", flag="0")]
+        
+      # identify the column types needed for stage file in SQL
+        abcd.stage.cols <- data.table(col.name = names(table_config$vars), col.type = table_config$vars)[col.name %in% names(abcd)]
+        abcd.characters <- abcd.stage.cols[tolower(col.type) %like% "char", ]$col.name
+        abcd.integers <- abcd.stage.cols[tolower(col.type) %like% "int", ]$col.name
+        abcd.dates <- abcd.stage.cols[tolower(col.type) %like% "date", ]$col.name
+        rm(abcd.stage.cols)
+    
+      # change the class of the R columns if necessary
+        abcd[, (abcd.characters):= lapply(.SD, as.character), .SDcols = abcd.characters]
+        abcd[, (abcd.integers):= lapply(.SD, as.integer), .SDcols = abcd.integers]
+        abcd[, (abcd.dates):= lapply(.SD, as.Date), .SDcols = abcd.dates]
+
+    
+## (5) Append MBSF AB & ABCD ----    
     abcd <- rbindlist(list(abcd, ab), fill = TRUE, use.names = TRUE)
     rm(ab)
     gc()
     
-## (5) Identify potential duplicates ----
+## (6) Identify potential duplicates ----
     begin.time <- Sys.time()
     abcd[, id_lowercase := tolower(bene_id)]
     by.cols = setdiff(names(abcd), 'bene_id') # all columns, except original case sensitive id column
@@ -111,7 +138,10 @@
     Sys.time() - begin.time
     rm(begin.time)
     
-## (6) PUSH MBSF combined AB/ABCD ----
+    if(nrow(mbsf.duplicates)>0)
+      stop("There are potential duplicates. Please type 'View(mbsf.duplicates)' and manually review these data")
+    
+## (7) PUSH MBSF combined AB/ABCD ----
     # create last_run timestamp
     abcd[, last_run := Sys.time()]
     
@@ -130,22 +160,22 @@
     dbWriteTable(db_claims, 
                  tbl_id, 
                  value = as.data.frame(abcd),
-                 overwrite = T, append = F, 
+                 overwrite = F, append = T, 
                  field.types = unlist(table_config$vars))
     
     # Confirm that all rows were loaded to sql
     stage.count <- as.numeric(odbc::dbGetQuery(db_claims, 
                                                 "SELECT COUNT (*) FROM stage.mcare_mbsf"))
-    if(stage.count != nrow(abcd))
+    if(stage.count != sum(count.stage$count) + nrow(abcd))
       stop("Mismatching row count, error reading in data")
 
-## (7) QA combined MBSF ----
+## (8) QA combined MBSF ----
     # Extract staged data from SQL ----
       last_run <- as.POSIXct(odbc::dbGetQuery(db_claims, "SELECT MAX (last_run) FROM stage.mcare_mbsf")[[1]])
       
-      raw.count <- ab.raw.count + abcd.raw.count
+      raw.count <- ab.raw.count + abcd.raw.count + sum(count.stage$count) # # rows in NEW AB + # rows in NEW ABCD + original # rows in Stage
       
-      row_diff <- raw.count - stage.count
+      row_diff <-  raw.count - stage.count # original # rows in stage + # new rows - current # rows in stage ... should equal zero
     
     # check that rows in stage are not more than rows in load_raw ----
     if(row_diff<0){
@@ -181,7 +211,7 @@
       
       problem.raw.row.count <- glue::glue(" ") # no problem, so empty error message
       
-    }# close check that load raw doesn't have less rows than stage
+    } # close check that load raw doesn't have less rows than stage
     
     # check that rows in stage are not less than the last time that it was created ----
       # count number of rows
@@ -293,7 +323,7 @@
       rm(abcd, last_run, previous_rows, problem.ids, problem.raw.row.count, problem.row_diff, row_diff, tbl_id, table_config)
       gc()        
       
-## (8) Fill qa_mcare_values table ----
+## (9) Fill qa_mcare_values table ----
       qa.values <- glue::glue_sql("INSERT INTO metadata.qa_mcare_values
                              (table_name, qa_item, qa_value, qa_date, note) 
                                      VALUES ('stage.mcare_mbsf',
@@ -306,7 +336,7 @@
       odbc::dbGetQuery(conn = db_claims, qa.values)
       
       
-## (9) Print error messages ----
+## (10) Print error messages ----
       if(problems >1){
         message(glue::glue("WARNING ... MBSF ABCD FAILED AT LEAST ONE QA TEST", "\n",
                            "Summary of problems in MBSF ABCD: ", "\n", 
