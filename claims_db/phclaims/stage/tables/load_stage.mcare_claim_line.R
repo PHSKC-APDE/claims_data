9703dea994f3415882e268b414102fb8f015a555
Eli Kern <eli.kern@kingcounty.gov>
2020-01-29 12:21:41 -0800

    Added file type and updated QA code

--- claims_db/phclaims/stage/tables/load_stage.mcare_claim_line.R
+++ claims_db/phclaims/stage/tables/load_stage.mcare_claim_line.R
@@ -16,7 +16,7 @@ load_stage.mcare_claim_line_f <- function() {
     --value per claim line.
     --Eli Kern (PHSKC-APDE)
     --2020-01
-    --Run time: 22 min
+    --Run time: XX min
     
     ------------------
     --STEP 1: Select (distinct) desired columns from multi-year claim tables on stage schema
@@ -26,11 +26,13 @@ load_stage.mcare_claim_line_f <- function() {
     
     --bcarrier
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'carrier' as filetype_mcare,
     revenue_code = null,
     a.place_of_service_code,
     a.type_of_service,
@@ -48,11 +50,13 @@ load_stage.mcare_claim_line_f <- function() {
     --dme
     union
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'dme' as filetype_mcare,
     revenue_code = null,
     a.place_of_service_code,
     a.type_of_service,
@@ -73,11 +77,13 @@ load_stage.mcare_claim_line_f <- function() {
     --hospice
     union
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'hospice' as filetype_mcare,
     a.revenue_code,
     place_of_service_code = null,
     type_of_service = null,
@@ -95,11 +101,13 @@ load_stage.mcare_claim_line_f <- function() {
     --inpatient
     union
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'inpatient' as filetype_mcare,
     a.revenue_code,
     place_of_service_code = null,
     type_of_service = null,
@@ -117,11 +125,13 @@ load_stage.mcare_claim_line_f <- function() {
     --outpatient
     union
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'outpatient' as filetype_mcare,
     a.revenue_code,
     place_of_service_code = null,
     type_of_service = null,
@@ -139,11 +149,13 @@ load_stage.mcare_claim_line_f <- function() {
     --snf
     union
     select
+    --top 100
     rtrim(a.id_mcare) as id_mcare,
     rtrim(a.claim_header_id) as claim_header_id,
     a.claim_line_id,
     b.first_service_date,
     b.last_service_date,
+    'snf' as filetype_mcare,
     a.revenue_code,
     place_of_service_code = null,
     type_of_service = null,
@@ -163,18 +175,64 @@ load_stage.mcare_claim_line_f <- function() {
 #### Table-level QA script ####
 qa_stage.mcare_claim_line_qa_f <- function() {
   
-  #load expected counts from YAML file
-  table_config <- yaml::yaml.load(RCurl::getURL(config_url))  
-  
-  #confirm that row counts match expected after excluding denied claims
+  #confirm that claim line counts match for a specific revenue code
   res1 <- dbGetQuery(conn = db_claims, glue_sql(
-  "select 'stage.mcare_claim_line' as 'table', 'row count, expect {table_config$row_count_expected}' as qa_type,
+  "select 'stage.mcare_claim_line' as 'table', 'row count, expect match with inpatient table' as qa_type,
   count(*) as qa
-  from stage.mcare_claim_line;",
+  from stage.mcare_claim_line
+  where file_type = 'inpatient' and revenue_code = '0450';",
   .con = db_claims))
   
-  #make sure everyone is in elig_demo
   res2 <- dbGetQuery(conn = db_claims, glue_sql(
+  "select 'stage.mcare_inpatient_revenue_center' as 'table', 'row count, expect match with claim_line table' as qa_type,
+  count(*) as qa
+  from (
+    select distinct a.id_mcare, a.claim_header_id, a.claim_line_id, b.first_service_date,
+    b.last_service_date, a.revenue_code
+    from stage.mcare_inpatient_revenue_center as a
+    left join stage.mcare_inpatient_base_claims as b
+    on a.claim_header_id = b.claim_header_id
+    left join PHClaims.final.mcare_elig_demo as c
+    on a.id_mcare = c.id_mcare
+    --exclude denined claims using carrier/dme claim method
+    where b.denial_code_facility = '' or b.denial_code_facility is null
+    --exclude claims among people who have eligibility data
+    and c.id_mcare is not null
+    --specific revenue code
+    and a.revenue_code = '0450'
+  ) as x;",
+  .con = db_claims))
+  
+  #confirm that claim line counts match for a specific place of service code
+  res3 <- dbGetQuery(conn = db_claims, glue_sql(
+    "select 'stage.mcare_claim_line' as 'table', 'row count, expect match with carrier table' as qa_type,
+    count(*) as qa
+    from stage.mcare_claim_line
+    where file_type = 'carrier' and place_of_service_code = '81';",
+    .con = db_claims))
+  
+  res4 <- dbGetQuery(conn = db_claims, glue_sql(
+    "select 'stage.mcare_bcarrier_line' as 'table', 'row count, expect match with claim_line table' as qa_type,
+    count(*) as qa
+    from (
+    	select distinct a.id_mcare, a.claim_header_id, a.claim_line_id, b.first_service_date,
+    	b.last_service_date, a.place_of_service_code, a.type_of_service
+    	from stage.mcare_bcarrier_line as a
+    	left join stage.mcare_bcarrier_claims as b
+    	on a.claim_header_id = b.claim_header_id
+    	left join PHClaims.final.mcare_elig_demo as c
+    	on a.id_mcare = c.id_mcare
+    	--exclude denined claims using carrier/dme claim method
+    	where b.denial_code in ('1','2','3','4','5','6','7','8','9')
+    	--exclude claims among people who have eligibility data
+    	and c.id_mcare is not null
+    	--specific place of service code
+    	and a.place_of_service_code = '81'
+    ) as x;",
+    .con = db_claims))
+
+  #make sure everyone is in elig_demo
+  res5 <- dbGetQuery(conn = db_claims, glue_sql(
   "select 'stage.mcare_claim_line' as 'table', '# members not in elig_demo, expect 0' as qa_type,
     count(a.id_mcare) as qa
     from stage.mcare_claim_line as a
@@ -184,7 +242,7 @@ qa_stage.mcare_claim_line_qa_f <- function() {
   .con = db_claims))
   
   #make sure everyone is in elig_timevar
-  res3 <- dbGetQuery(conn = db_claims, glue_sql(
+  res6 <- dbGetQuery(conn = db_claims, glue_sql(
   "select 'stage.mcare_claim_line' as 'table', '# members not in elig_timevar, expect 0' as qa_type,
     count(a.id_mcare) as qa
     from stage.mcare_claim_line as a
