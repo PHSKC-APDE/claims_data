2997465de24eef068d5ee1ffe8ed467bf49e4724
sdawadi <49413499+sdawadi@users.noreply.github.com>
2020-02-10 14:22:51 -0800

    upadating 2017 bcarrier line codeblock

--- claims_db/phclaims/stage/tables/archive/load_stage.mcare_bcarrier_line.sql
+++ claims_db/phclaims/stage/tables/archive/load_stage.mcare_bcarrier_line.sql
@@ -3,12 +3,18 @@
 --Eli Kern (PHSKC-APDE)
 --2019-12
 --Run time: 25 min
+--------------------------
+--------------------------
+--Shuva update
+--2/10/2020
+--added 2017 codeblock 
 
 
 insert into PHClaims.stage.mcare_bcarrier_line_load with (tablock)
 
 --2014 data
 select
+top 100 
 encrypted723beneficiaryid as id_mcare
 ,encryptedclaimid as claim_header_id
 ,claimlinenumber as claim_line_id
@@ -31,6 +37,7 @@ from PHClaims.load_raw.mcare_bcarrier_line_j_14
 --2015 data
 union
 select
+top 100
 encrypted723beneficiaryid as id_mcare
 ,encryptedclaimid as claim_header_id
 ,claimlinenumber as claim_line_id
@@ -53,6 +60,7 @@ from PHClaims.load_raw.mcare_bcarrier_line_k_15
 --2016 data
 union
 select
+top 100
 bene_id as id_mcare
 ,clm_id as claim_header_id
 ,line_num as claim_line_id
@@ -70,4 +78,31 @@ bene_id as id_mcare
 ,betos_cd as procedure_code_betos
 ,physn_zip_cd as provider_billing_zip
 ,getdate() as last_run
-from PHClaims.load_raw.mcare_bcarrier_line_k_16;
+from PHClaims.load_raw.mcare_bcarrier_line_k_16
+
+
+
+--2017 data
+union
+select
+top 100
+bene_id as id_mcare
+,clm_id as claim_header_id
+,line_num as claim_line_id
+,prf_physn_npi as provider_rendering_npi
+,org_npi_num as provider_org_npi
+,carr_line_prvdr_type_cd as provider_rendering_type
+,tax_num as provider_rendering_tin
+,prvdr_zip as provider_rendering_zip
+,prvdr_spclty as provider_rendering_specialty
+,line_cms_type_srvc_cd as type_of_service
+,line_place_of_srvc_cd as place_of_service_code
+,hcpcs_cd as procedure_code_hcpcs
+,hcpcs_1st_mdfr_cd as procedure_code_hcps_modifier_1
+,hcpcs_2nd_mdfr_cd as procedure_code_hcps_modifier_2
+,betos_cd as procedure_code_betos
+,physn_zip_cd as provider_billing_zip
+,getdate() as last_run
+from PHClaims.load_raw.mcare_bcarrier_line_k_17;
+
+
