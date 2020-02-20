7fdcd20034a4896eb832c612c6270a83d05e6bf4
Danny Colombara <dcolombara@kingcounty.gov>
2020-02-20 11:14:33 -0800

    added QA scripts to master_mcaid_mcare

--- claims_db/db_loader/mcaid_mcare/master_mcaid_mcare_analytic.R
+++ claims_db/db_loader/mcaid_mcare/master_mcaid_mcare_analytic.R
@@ -33,10 +33,10 @@ devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/m
 
 #### IDENTITY LINKAGE ####
 # Make stage version of linkage
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.xwalk_apde_mcaid_mcare_pha.r")
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.xwalk_apde_mcaid_mcare_pha.R")
 
 # QA stage version
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/qa_stage.xwalk_apde_mcaid_mcare_pha.r")
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/qa_stage.xwalk_apde_mcaid_mcare_pha.R")
 qa_xwalk_apde_mcaid_mcare_pha_f(conn = db_claims, load_only = F)
 
 # Alter schema to final table (currently hard coded, use YAML eventually)
@@ -57,8 +57,10 @@ DBI::dbExecute(db_claims,
 # Create and load stage
 devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/load_stage.mcaid_mcare_elig_demo.R")
 
-# QA and load to final
-# (no QA outside of what is in the stage code right now)
+# QA stage version
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/stage/tables/qa_stage.mcaid_mcare_elig_demo.R")
+qa_xwalk_apde_mcaid_mcare_pha_f(conn = db_claims, load_only = F)
+
 # Alter schema to final table (currently hard coded, use YAML eventually)
 alter_schema_f(conn = db_claims, 
                from_schema = "stage", 
