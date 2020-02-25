51428073980622ad0fc6b6b71dbb5186bd4a29b5
psylling <philip.sylling@gmail.com>
2020-02-25 07:56:00 -0800

    Update PQI scripts

--- claims_db/phclaims/stage/sprocs/create_stage.v_mcaid_pqi_ed.sql
+++ claims_db/phclaims/stage/sprocs/create_stage.v_mcaid_pqi_ed.sql
@@ -19,7 +19,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -42,7 +42,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -69,7 +69,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -104,7 +104,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -160,7 +160,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -195,7 +195,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 
@@ -257,7 +257,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 /*
@@ -318,7 +318,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 
@@ -342,7 +342,7 @@ SELECT
 ,a.[ed_pophealth_id]
 ,1 AS [flag]
 
-FROM [stage].[mcaid_claim_header] AS a
+FROM [final].[mcaid_claim_header] AS a
 INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[primary_diagnosis] = b.[code]
 
@@ -407,7 +407,7 @@ AND b.[value_set_name] IN ('ACSLEAD')
 ) AS a
 
 -- Claim has to be a valid inpatient claim that is not a transfer (by [admsn_source])
-INNER JOIN [stage].[mcaid_claim_header] AS b
+INNER JOIN [final].[mcaid_claim_header] AS b
 ON a.[claim_header_id] = b.[claim_header_id]
 AND b.[ed_pophealth_id] IS NOT NULL
 
@@ -437,11 +437,22 @@ INNER JOIN [ref].[ahrq_value_set] AS b
 ON a.[drvd_drg_code] = b.[code]
 AND b.[value_set_name] IN ('MDC 14')
 )
+),
+
+[aggregate_claim_header_id] AS
+(
+SELECT
+ [value_set_group]
+,[ed_pophealth_id]
+,MAX([flag]) AS [flag]
+FROM [get_pqi_claims]
+GROUP BY
+ [value_set_group]
+,[ed_pophealth_id]
 )
 
 SELECT
- [claim_header_id]
-,[ed_pophealth_id]
+ [ed_pophealth_id]
 ,ISNULL([PQI 01], 0) AS [pqi_01]
 ,ISNULL([PQI 03], 0) AS [pqi_03]
 ,ISNULL([PQI 05], 0) AS [pqi_05]
@@ -464,7 +475,7 @@ SELECT
  ISNULL([PQI 15], 0) +
  ISNULL([PQI 16], 0)) >= 1 THEN 1 ELSE 0 END AS [pqi_composite]
 
-FROM [get_pqi_claims]
+FROM [aggregate_claim_header_id]
 PIVOT(MAX([flag]) FOR [value_set_group] IN 
 ([PQI 01]
 ,[PQI 03]
