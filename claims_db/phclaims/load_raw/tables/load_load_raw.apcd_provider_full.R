3a7ee2fd1eb9eff45b3d4ab9c4bb07a3d130d83e
Eli Kern <eli.kern@kingcounty.gov>
2021-01-27 12:12:47 -0800

    Fixing server parameter

--- claims_db/phclaims/load_raw/tables/load_load_raw.apcd_provider_full.R
+++ claims_db/phclaims/load_raw/tables/load_load_raw.apcd_provider_full.R
@@ -53,7 +53,7 @@ load_load_raw.apcd_provider_full_f <- function(etl_date_min = NULL,
   load_table_from_file_f(conn = db_claims,
                          config_url = paste0("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.",
                                            table_name_part, "_full.yaml"),
-                         overall = F, ind_yr = T, combine_yr = T, test_mode = F, server = "phclaims")
+                         overall = F, ind_yr = T, combine_yr = T, test_mode = F, server = NULL)
   
   
   #### ADD BATCH ID COLUMN ####
