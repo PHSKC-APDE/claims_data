84e4f6db30cbfdd89e8f3ccf52909cf30b92f141
Whitehurst <jwhitehurst@ph.lcl>
2021-12-16 15:18:27 -0800

    more minor fixes for bak tables

--- claims_db/phclaims/stage/tables/load_stage.mcaid_elig.R
+++ claims_db/phclaims/stage/tables/load_stage.mcaid_elig.R
@@ -99,15 +99,21 @@ load_stage.mcaid_elig_f <- function(conn_db = NULL,
   # Check that the stage table actually exists so we don't accidentally wipe the archive table
   if (full_refresh == F & DBI::dbExistsTable(conn_dw, DBI::Id(schema = to_schema, table = to_table))) {
     if (server == "hhsaw") {
-      try(DBI::dbSendQuery(conn_dw, 
-                           glue::glue("DROP TABLE {`to_schema`}.{`paste0(archive_table, '_bak')`}")))
+      if(DBI::dbExistsTable(conn_dw, DBI::Id(schema = to_schema, table = paste0(archive_table, '_bak')))) {
+        try(DBI::dbSendQuery(conn_dw, 
+                             glue::glue_sql("DROP TABLE {`to_schema`}.{`paste0(archive_table, '_bak')`}", 
+                                            .con = conn_dw)))
+      }
       try(DBI::dbSendQuery(conn_dw, 
                            glue::glue("RENAME OBJECT {`to_schema`}.{`archive_table`} TO {`paste0(archive_table, '_bak')`}")))
       DBI::dbSendQuery(conn_dw, 
                        glue::glue("RENAME OBJECT {`to_schema`}.{`to_table`} TO {`archive_table`}"))
     } else if (server == "phclaims") {
-      try(DBI::dbSendQuery(conn_db, 
-                           glue::glue("DROP TABLE {`archive_schema`}.{`paste0(archive_table, '_bak')`}")))
+      if(DBI::dbExistsTable(conn_dw, DBI::Id(schema = archive_schema, table = paste0(archive_table, '_bak')))) {
+        try(DBI::dbSendQuery(conn_db, 
+                             glue::glue_sql("DROP TABLE {`archive_schema`}.{`paste0(archive_table, '_bak')`}", 
+                                            .con = conn_dw)))
+      }
       try(DBI::dbSendQuery(conn_db, 
                            glue::glue("EXEC sp_rename '{archive_schema}.{archive_table}',  '{paste0(archive_table, '_bak')}'")))
       alter_schema_f(conn = conn_db, 
