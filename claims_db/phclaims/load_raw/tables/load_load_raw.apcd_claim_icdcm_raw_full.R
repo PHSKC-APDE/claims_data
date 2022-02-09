787e4876ece26d55e1a07eebe4b14e83a864e89a
Eli Kern <eli.kern@kingcounty.gov>
2022-02-08 16:46:21 -0800

    Remove test_mode parameter

--- claims_db/phclaims/load_raw/tables/load_load_raw.apcd_claim_icdcm_raw_full.R
+++ claims_db/phclaims/load_raw/tables/load_load_raw.apcd_claim_icdcm_raw_full.R
@@ -59,7 +59,7 @@ load_load_raw.apcd_icdcm_full_f <- function(etl_date_min = NULL,
   load_table_from_file(conn = db_claims,
                          config_url = paste0("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/phclaims/load_raw/tables/load_load_raw.",
                                            table_name_part, "_full.yaml"),
-                         overall = F, ind_yr = T, combine_yr = T, test_mode = F, server = server)
+                         overall = F, ind_yr = T, combine_yr = T, server = server)
   
   
   #### ADD BATCH ID COLUMN ####
