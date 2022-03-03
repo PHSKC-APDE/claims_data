ca29df0d10e853648dc046071f73b3da1123c1e0
Eli Kern <eli.kern@kingcounty.gov>
2022-03-03 08:59:36 -0800

    Adding new raw provider ID to claim_provider table

--- claims_db/phclaims/stage/tables/load_stage.apcd_claim_provider.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_claim_provider.R
@@ -17,7 +17,7 @@ load_stage.apcd_claim_provider_f <- function() {
     -------------------
     insert into PHClaims.stage.apcd_claim_provider with (tablock)
     select internal_member_id as id_apcd, medical_claim_header_id as claim_header_id, first_service_dt as first_service_date, 
-    last_service_dt as last_service_date, provider_id_apcd, provider_type, getdate() as last_run
+    last_service_dt as last_service_date, provider_id_apcd, provider_id_raw_apcd, provider_type, getdate() as last_run
     from PHClaims.stage.apcd_claim_provider_raw;",
     .con = db_claims))
 }
