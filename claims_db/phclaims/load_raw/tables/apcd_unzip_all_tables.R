6d645cd695d08572cb296be62312f187edd8f9f7
Eli Kern <eli.kern@kingcounty.gov>
2022-04-21 22:07:53 +0000

    Create apcd_unzip_all_tables.R

---
+++ claims_db/phclaims/load_raw/tables/apcd_unzip_all_tables.R
@@ -0,0 +1,14 @@
+# Eli Kern
+# APDE, PHSKC
+# 2019-1-28
+
+#### Uncompress WA-APCD GZIP files from Analytic Enclave ####
+
+library(R.utils)
+filelist <- list.files("//kcitsqlutpdbh51/ImportData/Data/APCD_data_import", 
+                       pattern = "\\.gz$",
+                       recursive = T,,
+                       full.names = T)
+for(file in filelist) {
+  gunzip(file, remove = F)
+}
