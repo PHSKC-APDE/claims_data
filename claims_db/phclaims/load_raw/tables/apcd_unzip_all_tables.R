4eaa02353ca932b71484b4d78685eec55ce0c960
Eli Kern <eli.kern@kingcounty.gov>
2022-04-25 16:11:29 +0000

    Update apcd_unzip_all_tables.R

--- claims_db/phclaims/load_raw/tables/apcd_unzip_all_tables.R
+++ claims_db/phclaims/load_raw/tables/apcd_unzip_all_tables.R
@@ -7,7 +7,7 @@
 library(R.utils)
 filelist <- list.files("//kcitsqlutpdbh51/ImportData/Data/APCD_data_import", 
                        pattern = "\\.gz$",
-                       recursive = T,,
+                       recursive = T,
                        full.names = T)
 for(file in filelist) {
   gunzip(file, remove = F)
