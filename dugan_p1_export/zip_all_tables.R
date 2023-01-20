8a9e515271f49a73545fa4312ba2bc1a37799b4b
Eli Kern <eli.kern@kingcounty.gov>
2023-01-20 09:33:00 -0800

    Adding scripts for P1 export for Dugan project

---
+++ dugan_p1_export/zip_all_tables.R
@@ -0,0 +1,14 @@
+# Eli Kern
+# APDE, PHSKC
+# 2023-01
+
+#### Compress P1 data CSV files as GZIP files for Dugan project ####
+
+library(R.utils)
+filelist <- list.files("//phcifs.ph.lcl/SFTP_DATA/APDEDataExchange/UW_Dugan_Team/Staging", 
+                       pattern = "\\.csv$",
+                       recursive = T,
+                       full.names = T)
+for(file in filelist) {
+  gzip(file, remove = F, overwrite = T)
+}
