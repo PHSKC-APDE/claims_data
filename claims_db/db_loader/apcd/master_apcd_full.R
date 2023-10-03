c54ba2420b77389fc267fb9b43bdd83b6598a2a4
Susan Hernandez <49567222+seh315@users.noreply.github.com>
2023-10-03 15:30:04 -0700

    update server name

--- claims_db/db_loader/apcd/master_apcd_full.R
+++ claims_db/db_loader/apcd/master_apcd_full.R
@@ -38,9 +38,9 @@ devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/m
 
 #### SET UP VALUES FOR ETL LOG ####
 etl_date_min <- "2014-01-01"
-etl_date_max <- "2022-12-31" #UPDATE EACH QUARTER
+etl_date_max <- "2023-03-31" #UPDATE EACH QUARTER
 etl_delivery_date <- "2023-06-12" #UPDATE EACH QUARTER , date downloaded it to our KCIT server.
-etl_note <- "Full refresh of APCD data using extract 10013" #UPDATE EACH QUARTER
+etl_note <- "Full refresh of APCD data using extract 10015" #UPDATE EACH QUARTER
 
 ## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ##
 #### STEP 1: Load and QA new raw data to load_raw schema, and reference tables to ref schema ####
