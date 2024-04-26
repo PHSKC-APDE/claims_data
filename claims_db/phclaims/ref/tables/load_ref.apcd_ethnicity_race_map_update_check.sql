eb367dee2d54b1f4aec28717df4bd20ee608eb3c
Eli Kern <eli.kern@kingcounty.gov>
2024-04-26 09:00:07 -0700

    Updating table names with cci suffix

--- claims_db/phclaims/ref/tables/load_ref.apcd_ethnicity_race_map_update_check.sql
+++ claims_db/phclaims/ref/tables/load_ref.apcd_ethnicity_race_map_update_check.sql
@@ -2,7 +2,7 @@
 --run this code to identify ethnicities in APCD data that are missing from ethnicity_race crosswalk
 --1st ethnicity variable
 select distinct a.ethnicity_id1, b.ethnicity_desc, b.race_id, b.race_desc
-from claims.stage_apcd_eligibility as a
+from claims.stage_apcd_eligibility_cci as a
 left join claims.ref_apcd_ethnicity_race_map as b
 on a.ethnicity_id1 = cast(b.ethnicity_id as bigint)
 order by a.ethnicity_id1;
@@ -10,7 +10,7 @@ order by a.ethnicity_id1;
 --2nd ethnicity variable
 --run this code to identify ethnicities in APCD data that are missing from ethnicity_race crosswalk
 select distinct a.ethnicity_id2, b.ethnicity_desc, b.race_id, b.race_desc
-from claims.stage_apcd_eligibility as a
+from claims.stage_apcd_eligibility_cci as a
 left join claims.ref_apcd_ethnicity_race_map as b
 on a.ethnicity_id2 = cast(b.ethnicity_id as bigint)
 order by a.ethnicity_id2;
