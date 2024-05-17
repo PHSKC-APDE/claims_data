c7963e07a10f37ccdd8af31fb94e2fc0d59f863a
Eli Kern <eli.kern@kingcounty.gov>
2024-05-17 11:31:40 -0700

    Create pharm char table

---
+++ claims_db/phclaims/stage/tables/load_stage.mcare_claim_pharm_char.R
@@ -0,0 +1,45 @@
+#### CODE TO LOAD & TABLE-LEVEL QA STAGE.MCARE_claim_pharm_char
+# Eli Kern, PHSKC (APDE)
+#
+# 2024-05
+
+### Run from 03_master_mcare_claims_analytic.R script
+# https://github.com/PHSKC-APDE/claims_data/blob/main/claims_db/db_loader/mcare/03_master_mcare_claims_analytic.R
+
+#### Load script ####
+load_stage.mcare_claim_pharm_char_f <- function() {
+  
+  ### Run SQL query
+  odbc::dbGetQuery(dw_inthealth, glue::glue_sql(
+    "insert into stg_claims.stage_mcare_claim_pharm_char
+    select
+    ncpdp_id as pharmacy_id,
+    physical_location_state_code,
+    physical_location_open_date,
+    physical_location_close_date,
+    dispenser_class,
+    primary_dispenser_type,
+    primary_taxonomy_code,
+    secondary_dispenser_type,
+    secondary_taxonomy_code,
+    tertiary_dispenser_type,
+    tertiary_taxonomy_code,
+    relationship_id,
+    relationship_from_dt,
+    relationship_thru_dt,
+    relationship_type,
+    prnt_org_id,
+    eprscrb_srvc_ind,
+    eprscrb_srvc_cd,
+    dme_srvc_ind,
+    dme_srvc_cd,
+    walkin_clinic_ind,
+    walkin_clinic_cd,
+    immunizations_ind,
+    immunizations_cd,
+    status_340b_ind,
+    status_340b_cd,
+    getdate() as last_run
+    from stg_claims.mcare_pharm_char;",
+        .con = dw_inthealth))
+}
