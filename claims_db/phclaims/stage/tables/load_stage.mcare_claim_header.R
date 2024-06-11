316bb9535ea111b86da2109c78b7e7c4ad52a5a1
Eli Kern <eli.kern@kingcounty.gov>
2024-06-10 23:16:23 -0700

    Update load_stage.mcare_claim_header.R

--- claims_db/phclaims/stage/tables/load_stage.mcare_claim_header.R
+++ claims_db/phclaims/stage/tables/load_stage.mcare_claim_header.R
@@ -986,7 +986,7 @@ qa_stage.mcare_claim_header_qa_f <- function() {
     select * 
     ,lag(ed_pophealth_id) over(partition by id_mcare, ed_pophealth_id order by first_service_date) as lag_ed_pophealth_id
     ,lag(first_service_date) over(partition by id_mcare, ed_pophealth_id order by first_service_date) as lag_first_service_date
-    from stg_claims.stage_apcd_claim_header
+    from stg_claims.stage_mcare_claim_header
     where ed_pophealth_id is not null
     )
     select 'stg_claims.stage_mcare_claim_header' as 'table', '# of ed_pophealth visits where the overlap date is greater than 1 day, expect 0' as 'qa_type',
