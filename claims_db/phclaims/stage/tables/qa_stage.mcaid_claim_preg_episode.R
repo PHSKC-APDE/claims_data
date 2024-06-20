d6b16f942ae0be364f6bc8f925811e59895b08ed
jwhitehurst-kc <jwhitehurst@kingcounty.gov>
2024-06-20 11:29:02 -0700

    adding new qa items

--- claims_db/phclaims/stage/tables/qa_stage.mcaid_claim_preg_episode.R
+++ claims_db/phclaims/stage/tables/qa_stage.mcaid_claim_preg_episode.R
@@ -106,7 +106,168 @@ qa_stage_mcaid_claim_preg_episode_f <- function(conn = NULL,
                                   .con = conn))
   }  
   
+  min_age <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(*) from {`to_schema`}.{`to_table`} where age_at_outcome < 12", .con = conn)))
+  if (min_age == 0) {
+    qa1 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Minimum age >= 12 as expected', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'Minimum age >= 12 as expected')",
+                                  .con = conn))
+  } else {
+    qa1 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Minimum age is under 12, lower than expected', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{min_age} row(s) with age lower than expected minimum (12)')",
+                                  .con = conn))
+  }
+           
+  max_age <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(*) from {`to_schema`}.{`to_table`} where age_at_outcome > 55", .con = conn)))
+  if (max_age == 0) {
+    qa2 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Maximum age <= 55 as expected', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'Maximum age <= 55 as expected')",
+                                  .con = conn))
+  } else {
+    qa2 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Maximum age is over 55, higher than expected', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{max_age} row(s) with age higher than expected maximum (55)')",
+                                  .con = conn))
+  }
+  
+  null_date <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(*) from {`to_schema`}.{`to_table`} where preg_start_date is null or preg_end_date is null;", .con = conn)))
+  if (null_date == 0) {
+    qa3 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'All rows with non-null start and end dates', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'All rows with non-null start and end dates')",
+                                  .con = conn))
+  } else {
+    qa3 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Rows with null start or end dates', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{null_date} row(s) with null start or end dates')",
+                                  .con = conn))
+  }
+  
+  null_ga <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(*) from {`to_schema`}.{`to_table`} where valid_ga = 1 and (ga_days is null or ga_weeks is null or ga_estimation_step is null);", .con = conn)))
+  if (null_ga == 0) {
+    qa4 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'All valid GA rows with non-null GA columns', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'All valid GA rows with non-null GA columns')",
+                                  .con = conn))
+  } else {
+    qa4 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Valid GA rows with null GA columns', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{null_ga} valid GA row(s) with null GA columns')",
+                                  .con = conn))
+  }
+  
+  end_types <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(distinct preg_endpoint) from {`to_schema`}.{`to_table`};", .con = conn)))
+  if (end_types == 7) {
+    qa5 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Expected # of distict preg endpoint types (7)', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'Expected # of distict preg endpoint types (7)')",
+                                  .con = conn))
+  } else {
+    qa5 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'Incorrect # of distict preg endpoint types (expecting 7)', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{end_tpes} distinct preg endpoint types (expecting 7)')",
+                                  .con = conn))
+  }
+  
+  null_lb <- as.integer(DBI::dbGetQuery(conn, glue::glue_sql("select count(*) from {`to_schema`}.{`to_table`} where preg_endpoint = 'lb' and valid_ga = 1 and lb_type is null;", .con = conn)))
+  if (null_lb == 0) {
+    qa6 <- 0
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'All LB records with valid GA with non-null lb_type', 
+                   'PASS', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   'All LB records with valid GA with non-null lb_type')",
+                                  .con = conn))
+  } else {
+    qa6 <- 1
+    DBI::dbExecute(conn = conn,
+                   glue::glue_sql("INSERT INTO {`qa_schema`}.{DBI::SQL(qa_table)}qa_mcaid
+                   (last_run, table_name, qa_item, qa_result, qa_date, note) 
+                   VALUES ({format(last_run, usetz = FALSE)}, 
+                   '{DBI::SQL(to_schema)}.{DBI::SQL(to_table)}',
+                   'LB records with valid GA with null lb_type', 
+                   'FAIL', 
+                   {format(Sys.time(), usetz = FALSE)}, 
+                   '{null_lb} LB row(s) with valid GA with null lb_type')",
+                                  .con = conn))
+  }
   #### SUM UP FAILURES ####
-  fail_tot <- sum(ids_fail)
+  fail_tot <- sum(ids_fail, qa1, qa2, qa3, qa4, qa5, qa6)
   return(fail_tot)
 }
