f1d703ef73ab0be335539319ec083bfd49a36d2a
Susan Hernandez <shernandez@kingcounty.gov>
2024-12-06 20:28:56 -0800

    updated with INTHealth schema

--- claims_db/phclaims/ref/tables/load_ref.apcd_ethnicity_race_map_update_check.sql
+++ claims_db/phclaims/ref/tables/load_ref.apcd_ethnicity_race_map_update_check.sql
@@ -2,16 +2,20 @@
 --run this code to identify ethnicities in APCD data that are missing from ethnicity_race crosswalk
 --1st ethnicity variable
 select distinct a.ethnicity_id1, b.ethnicity_desc, b.race_id, b.race_desc
-from claims.stage_apcd_eligibility_cci as a
-left join claims.ref_apcd_ethnicity_race_map as b
+--from claims.stage_apcd_eligibility_cci as a
+from [stg_claims].[apcd_eligibility] as a
+--left join claims.ref_apcd_ethnicity_race_map as b
+left join [stg_claims].[ref_apcd_ethnicity_race_map] as b
 on a.ethnicity_id1 = cast(b.ethnicity_id as bigint)
 order by a.ethnicity_id1;
 
 --2nd ethnicity variable
 --run this code to identify ethnicities in APCD data that are missing from ethnicity_race crosswalk
 select distinct a.ethnicity_id2, b.ethnicity_desc, b.race_id, b.race_desc
-from claims.stage_apcd_eligibility_cci as a
-left join claims.ref_apcd_ethnicity_race_map as b
+--from claims.stage_apcd_eligibility_cci as a
+from [stg_claims].[apcd_eligibility] as a
+--left join claims.ref_apcd_ethnicity_race_map as b
+left join[stg_claims].[ref_apcd_ethnicity_race_map] as b
 on a.ethnicity_id2 = cast(b.ethnicity_id as bigint)
 order by a.ethnicity_id2;
 
