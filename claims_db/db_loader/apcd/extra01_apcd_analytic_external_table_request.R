d4a5dbb3849e12d58fdb1f1640068b41faff9dbe
Susan Hernandez <shernandez@kingcounty.gov>
2026-02-19 13:30:12 -0800

    updated table list

--- claims_db/db_loader/apcd/extra01_apcd_analytic_external_table_request.R
+++ claims_db/db_loader/apcd/extra01_apcd_analytic_external_table_request.R
@@ -21,25 +21,10 @@ conn_ext <- create_db_connection("hhsaw", interactive = F, prod = T)
 ## Create list of tables on inthealth_edw for which I need an external table on HHSAW
 ## Update list as needed to replace list with new tables or tables that have had changes to column number/name/type
 tables <- data.frame(table_name = c(
-  "stage_apcd_claim_ccw",
-  "stage_apcd_claim_header",
-  "stage_apcd_claim_icdcm_header",
-  "stage_apcd_claim_line",
-  "stage_apcd_claim_preg_episode",
-  "stage_apcd_claim_procedure",
-  "stage_apcd_claim_provider",
   "stage_apcd_elig_demo",
-  "stage_apcd_elig_timevar",
-  "stage_apcd_elig_plr_2014",
-  "stage_apcd_elig_plr_2015",
-  "stage_apcd_elig_plr_2016",
-  "stage_apcd_elig_plr_2017",
-  "stage_apcd_elig_plr_2018",
-  "stage_apcd_elig_plr_2019",
-  "stage_apcd_elig_plr_2020",
-  "stage_apcd_elig_plr_2021",
-  "stage_apcd_elig_plr_2022",
-  "stage_apcd_elig_plr_2023"
+  "stage_apcd_elig_plr_2024",
+  "claims.final_apcd_claim_procedure",
+  "claims.final_apcd_claim_header"
 ))
 
 ## Use function to loop over table names and create SQL code
