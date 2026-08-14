9825c18c1fd92adcf61b650c886f1a3bbcb2cdb9
Eli Kern <eli.kern@kingcounty.gov>
2026-08-14 11:30:35 -0700

    Update to use apde.etl functions, copy Onpoint APCD ref tables from inthealth_edw to hhsaw

--- claims_db/db_loader/apcd/06_apcd_copy_ref_tables_inthealth_edw.R
+++ claims_db/db_loader/apcd/06_apcd_copy_ref_tables_inthealth_edw.R
@@ -1,9 +1,10 @@
-#### CODE TO COPY NEEDED ETL REFERENCE TABLES FROM HHSAW TO SYNAPSE
+#### CODE TO COPY NEEDED ETL REFERENCE TABLES BETWEEN INTHEALTH_EDW AND HHSAW
 # Eli Kern, PHSKC (APDE)
 #
 # 2024-05
 #10-16-2024 added keyring for inthealth
-#02-05-2026 update the deprecated function 
+#02-05-2026 update the deprecated function
+#08-14-2026 Eli updated to use apde.etl package, added section to copy Onpoint APCD ref tables from inthealth_edw to hhsaw
 
 # clean memory ----
 rm(list=ls())
@@ -19,7 +20,6 @@ origin <- "1970-01-01" # Date origin
 pacman::p_load(tidyverse, odbc, configr, glue, keyring, svDialogs, R.utils, apde.etl)
 
 #### STEP 1: Connect to SQL DATABASES ####
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/db_loader/mcaid/create_db_connection.R")
 #key_set("inthealth_edw_prod", username = "shernandez@kingcounty.gov") #Only run this each time password is changed
 interactive_auth <- FALSE
 prod <- TRUE
@@ -27,53 +27,39 @@ db_claims <- create_db_connection("hhsaw", interactive = interactive_auth, prod 
 dw_inthealth <- create_db_connection("inthealth", interactive = interactive_auth, prod = prod)
 keyring::key_list() #Confirm you have a key set for hhsaw and inthealth_edw_prod on this machine
 
-#Load Jeremy's table duplicate function
-#devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/apde/main/R/table_duplicate.R")
-#updated function
-devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/apde.etl/refs/heads/main/R/table_duplicate.R")
-
 
 #### STEP 2: Copy tables from HHSAW claims schema ####
 
 #Specify tables to be copied from claims schema of HHSAW
-tables_to_copy <- data.frame(
+tables_to_copy_from_hhsaw_claims <- data.frame(
     from_schema = ("claims"),
-    from_table = c("ref_apcd_claim_status",
-                   "ref_apcd_ethnicity_race_map",
-                   "ref_apcd_zip_group",
-                   "ref_ccw_lookup",
+    from_table = c("ref_ccw_lookup",
                    "ref_geo_county_code_wa",
                    "ref_geo_kc_zip",
                    "ref_kc_claim_type_crosswalk",
                    "ref_moll_preg_endpoint",
                    "ref_moll_trimester",
                    "ref_pc_visit_oregon",
-                   "ref_date",
-                   "ref_apcd_procedure_code"),
+                   "ref_date"),
     to_schema = ("stg_claims"),
-    to_table = c("ref_apcd_claim_status",
-                 "ref_apcd_ethnicity_race_map",
-                 "ref_apcd_zip_group",
-                 "ref_ccw_lookup",
+    to_table = c("ref_ccw_lookup",
                  "ref_geo_county_code_wa",
                  "ref_geo_kc_zip",
                  "ref_kc_claim_type_crosswalk",
                  "ref_moll_preg_endpoint",
                  "ref_moll_trimester",
                  "ref_pc_visit_oregon",
-                 "ref_date",
-                 "ref_apcd_procedure_code"),
+                 "ref_date"),
     stringsAsFactors = FALSE # prevents character columns in a data frame to be automatically converted into factors
     )
 
-
 #Run command
-system.time(table_duplicate(
+system.time(apde.etl::table_duplicate(
   conn_from = db_claims,
   conn_to = dw_inthealth,
   server_to = "inthealth_edw_prod",
   db_to = "inthealth_edw",
-  table_df = tables_to_copy,
+  table_df = tables_to_copy_from_hhsaw_claims,
   confirm_tables = TRUE,
   delete_table = TRUE
 ))
@@ -82,7 +68,7 @@ system.time(table_duplicate(
 #### Step 3: Copy tables from HHSAW ref schema ####
 
 #Specify tables to be copied from ref schema of HHSAW
-table_df_ref <- as.data.frame(
+tables_to_copy_from_hhsaw_ref <- as.data.frame(
   list(
     from_schema = c("ref"),
     from_table = c("icdcm_codes", "rda_value_sets_apde", "ndc_codes"),
@@ -92,12 +78,38 @@ table_df_ref <- as.data.frame(
 )
 
 #Run command
-system.time(table_duplicate(
+system.time(apde.etl::table_duplicate(
   conn_from = db_claims,
   conn_to = dw_inthealth,
   server_to = "inthealth_edw_prod",
   db_to = "inthealth_edw",
-  table_df = table_df_ref,
+  table_df = tables_to_copy_from_hhsaw_ref,
+  confirm_tables = TRUE,
+  delete_table = TRUE
+))
+
+
+#### Step 4: Copy Onpoint APCD ref tables from INTHEALTH_EDW to HHSAW ####
+
+#Generate vector of all Onpoint APCD ref tables
+onpoint_ref_tables <- DBI::dbListTables(dw_inthealth, schema = "stg_claims")
+onpoint_ref_tables <- onpoint_ref_tables[grepl("^ref_apcd", onpoint_ref_tables)]
+
+tables_to_copy_from_inthealth_edw <- data.frame(
+  from_schema = ("stg_claims"),
+  from_table = onpoint_ref_tables,
+  to_schema = ("claims"),
+  to_table = onpoint_ref_tables,
+  stringsAsFactors = FALSE # prevents character columns in a data frame to be automatically converted into factors
+)
+
+#Run command
+system.time(apde.etl::table_duplicate(
+  conn_from = dw_inthealth,
+  conn_to = db_claims,
+  server_to = "hhsaw",
+  db_to = "hhs_analytics_workspace",
+  table_df = tables_to_copy_from_inthealth_edw,
   confirm_tables = TRUE,
   delete_table = TRUE
 ))
