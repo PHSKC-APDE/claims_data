c8457da18f5d2df8b79671ce4f5196d4b43ad8a5
Eli Kern <eli.kern@kingcounty.gov>
2026-08-19 14:58:28 -0700

    changing creation of out of state flag to a more logical flow

--- claims_db/phclaims/stage/tables/load_stage.apcd_ref_nonresident_id.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_ref_nonresident_id.R
@@ -12,12 +12,12 @@ load_stage.apcd_ref_nonresident_id_f <- function() {
     "
     --flag out of state member months
     with temp1 as (
-      select a.id_apcd, a.year_month, case when b.zip_group_desc is null then 1 else 0 end as out_of_state
+      select a.id_apcd, a.year_month, case when b.state = 'WA' then 0 else 1 end as out_of_state
       from (
         select internal_member_id as id_apcd, year_month, zip_code
         from stg_claims.apcd_member_month_detail
       ) as a
-      left join (select * from stg_claims.ref_apcd_zip_group where zip_group_type_desc = 'County') as b
+      left join stg_claims.ref_apcd_zip as b
       on a.zip_code = b.zip_code
     ),
     --flag members that have no in-state WA member months
