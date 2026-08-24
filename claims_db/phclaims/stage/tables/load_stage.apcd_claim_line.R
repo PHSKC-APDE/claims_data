cdbb842f8704610f3de6573ec238d8ecb900259e
Eli Kern <eli.kern@kingcounty.gov>
2026-08-24 09:16:30 -0700

    Enclave -> KC ETL mods
    Merged Enclave and KC scripts, placeholder for charge_amt (set to NULL for now) until full cost info available

--- claims_db/phclaims/stage/tables/load_stage.apcd_claim_line.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_claim_line.R
@@ -5,6 +5,8 @@
 #
 # 2023-07-28 update: Corrected nonsensical discharge dates
 # 2024-04-29 update: Modified for HHSAW migration
+# 2026-08-24 update: Modified under migration of ETL from Enclave -> KC
+  # Added code to exclude i) non-WA residents, ii) people with no claims but no enrollment data, iii) set charge_amt to NULL for now
 
 ### Run from master_apcd_analytic script
 # https://github.com/PHSKC-APDE/claims_data/blob/main/claims_db/db_loader/apcd/07_apcd_create_analytic_tables.R
@@ -21,18 +23,18 @@ load_stage.apcd_claim_line_f <- function() {
     -------------------
     insert into stg_claims.stage_apcd_claim_line
     select distinct
-    a.id_apcd,
-    a.claim_header_id,
-    a.claim_line_id,
+    a.internal_member_id as id_apcd,
+    a.medical_claim_header_id as claim_header_id,
+    a.medical_claim_service_line_id as claim_line_id,
     a.line_counter,
     a.first_service_dt as first_service_date,
     a.last_service_dt as last_service_date,
-    a.charge_amt,
+    NULL as charge_amt, --interim placeholder until cost data allowed
     a.revenue_code,
     a.place_of_service_code,
     a.admission_dt as admission_date,
     
-     case
+    case
       when a.discharge_dt < a.admission_dt then a.last_service_dt
       when a.admission_dt is null and a.discharge_dt < a.first_service_dt then a.last_service_dt
       else a.discharge_dt
@@ -42,11 +44,19 @@ load_stage.apcd_claim_line_f <- function() {
     a.admission_point_of_origin_code,
     a.admission_type,
     getdate() as last_run
-    from stg_claims.apcd_claim_line_raw as a
-    --exclude denined/orphaned claims
+    from stg_claims.apcd_medical_claim as a
+    
     left join stg_claims.apcd_medical_claim_header as b
-    on a.claim_header_id = b.medical_claim_header_id
-    where b.denied_header_flag = 'N' and b.orphaned_header_flag = 'N';",
+    on a.medical_claim_header_id = b.medical_claim_header_id
+    left join stg_claims.apcd_ref_nonresident_id as y
+    on a.internal_member_id = y.id_apcd
+    left join stg_claims.apcd_ref_claim_no_elig as z
+    on a.internal_member_id = z.id_apcd
+    
+    --exclude denined/orphaned claims
+    where (b.denied_header_flag = 'N' and b.orphaned_header_flag = 'N')
+    --exclude members with no WA residency OR no elig data
+    and (y.id_apcd is null and z.id_apcd is null);",
     .con = dw_inthealth))
 }
 
@@ -63,12 +73,12 @@ qa_stage.apcd_claim_line_f <- function() {
     where b.id_apcd is null;",
     .con = dw_inthealth))
   
-  #make sure everyone is in elig_timevar
+  #make sure everyone is in elig_month
   res2 <- dbGetQuery(conn = dw_inthealth, glue_sql(
-    "select 'stg_claims.stage_apcd_claim_line' as 'table', '# members not in elig_timevar, expect 0' as qa_type,
+    "select 'stg_claims.stage_apcd_claim_line' as 'table', '# members not in elig_month, expect 0' as qa_type,
     count(a.id_apcd) as qa
     from stg_claims.stage_apcd_claim_line as a
-    left join stg_claims.stage_apcd_elig_timevar as b
+    left join stg_claims.stage_apcd_elig_month as b
     on a.id_apcd = b.id_apcd
     where b.id_apcd is null;",
     .con = dw_inthealth))
