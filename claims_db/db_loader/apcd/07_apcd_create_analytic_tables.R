f9b3b8511ffea0c08d5552afe5967538650a7667
Eli Kern <eli.kern@kingcounty.gov>
2026-08-24 09:16:40 -0700

    Set hash distribution for claim_line

--- claims_db/db_loader/apcd/07_apcd_create_analytic_tables.R
+++ claims_db/db_loader/apcd/07_apcd_create_analytic_tables.R
@@ -326,7 +326,8 @@ devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/m
 
 ### B) Create table
 create_table(conn = dw_inthealth, config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_line.yaml",
-             overall = T, ind_yr = F, overwrite = T, server = "hhsaw")
+             overall = T, ind_yr = F, overwrite = T, server = "hhsaw",
+             with = "DISTRIBUTION = HASH(claim_header_id)")
 
 ### C) Load tables
 system.time(load_stage.apcd_claim_line_f())
