b3652443daeba0a7a73ad8c6f724c8cfa2ae61dc
Eli Kern <eli.kern@kingcounty.gov>
2026-08-24 14:58:14 -0700

    Enclave -> KC ETL mods
      # Added code to exclude i) non-WA residents, ii) people with no claims but no enrollment data, iii) condensed code using PIVOT,
      # iv) source ICD procedure codes from medical_claim_icd_procedure table

--- claims_db/phclaims/stage/tables/load_stage.apcd_claim_procedure.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_claim_procedure.R
@@ -5,7 +5,10 @@
 #
 # 2024-04-29 update: Modified for HHSAW migration
 # 2025-04-02 update: Removed procedure_code_number column and consolidated modifier codes into single column
-
+# 2026-08-24 update: Modified under migration of ETL from Enclave -> KC
+  # Added code to exclude i) non-WA residents, ii) people with no claims but no enrollment data, iii) condensed code using PIVOT,
+  # iv) source ICD procedure codes from medical_claim_icd_procedure table
+  
 ### Run from master_apcd_analytic script
 # https://github.com/PHSKC-APDE/claims_data/blob/main/claims_db/db_loader/apcd/07_apcd_create_analytic_tables.R
 
@@ -15,19 +18,85 @@ load_stage.apcd_claim_procedure_f <- function() {
   ### Run SQL query
   odbc::dbGetQuery(dw_inthealth, glue::glue_sql(
     "
-    ------------------
-    --STEP 1: Create procedure code table with exclusions applied
-    --Exclude all denied and orphaned claim lines
-    -------------------
-    insert into stg_claims.stage_apcd_claim_procedure
-    select a.internal_member_id as id_apcd, a.medical_claim_header_id as claim_header_id,
-    	a.first_service_dt as first_service_date, a.last_service_dt as last_service_date, a.procedure_code, a.modifier_code,
-    	getdate() as last_run
-    from stg_claims.apcd_claim_procedure_raw as a
-    --exclude denined/orphaned claims
-    left join stg_claims.apcd_medical_claim_header as b
-    on a.medical_claim_header_id = b.medical_claim_header_id
-    where b.denied_header_flag = 'N' and b.orphaned_header_flag = 'N';",
+	--Extract HCPCS and CPT procedure codes
+	with line_procedure as (
+		--test code for line-level procedure codes
+		select
+		distinct
+		a.internal_member_id as id_apcd,
+		a.medical_claim_header_id as claim_header_id,
+		b.first_service_dt as first_service_date,
+		b.last_service_dt as last_service_date,
+		a.procedure_code,
+		v.modifier_code
+		from stg_claims.apcd_medical_claim as a
+		left join stg_claims.apcd_medical_claim_header as b
+		on a.medical_claim_header_id = b.medical_claim_header_id
+		left join stg_claims.apcd_ref_nonresident_id as y
+		on a.internal_member_id = y.id_apcd
+		left join stg_claims.apcd_ref_claim_no_elig as z
+		on a.internal_member_id = z.id_apcd
+		cross apply (
+			-- Valid modifiers
+			select a.procedure_modifier_code_1
+			where a.procedure_modifier_code_1 NOT IN ('-1','-2')
+
+			union all
+			select a.procedure_modifier_code_2
+			where a.procedure_modifier_code_2 NOT IN ('-1','-2')
+
+			union all
+			select a.procedure_modifier_code_3
+			where a.procedure_modifier_code_3 NOT IN ('-1','-2')
+
+			union all
+			select a.procedure_modifier_code_4
+			where a.procedure_modifier_code_4 NOT IN ('-1','-2')
+
+			-- No-modifier case: if ALL FOUR are invalid, produce a single NULL row
+			union all
+			select null
+			where a.procedure_modifier_code_1 IN ('-1','-2')
+			  and a.procedure_modifier_code_2 IN ('-1','-2')
+			  and a.procedure_modifier_code_3 IN ('-1','-2')
+			  and a.procedure_modifier_code_4 IN ('-1','-2')
+		) v(modifier_code)
+		where a.procedure_code not in ('-1','-2')
+		--exclude denined/orphaned claims
+		and (b.denied_header_flag = 'N' and b.orphaned_header_flag = 'N')
+		--exclude members with no WA residency OR no elig data
+		and (y.id_apcd is null and z.id_apcd is null)
+	),
+	--Extract ICD procedure codes
+	icd_procedure as (
+		--test code for ICD procedure codes
+		select
+		distinct
+		a.internal_member_id as id_apcd,
+		c.medical_claim_header_id as claim_header_id,
+		c.first_service_dt as first_service_date,
+		c.last_service_dt as last_service_date,
+		a.icd_procedure_code as procedure_code,
+		null as modifier_code
+		from stg_claims.apcd_medical_claim_icd_procedure as a
+		left join stg_claims.apcd_medical_claim as b
+		on a.medical_claim_service_line_id = b.medical_claim_service_line_id
+		left join stg_claims.apcd_medical_claim_header as c
+		on b.medical_claim_header_id = c.medical_claim_header_id
+		left join stg_claims.apcd_ref_nonresident_id as y
+		on a.internal_member_id = y.id_apcd
+		left join stg_claims.apcd_ref_claim_no_elig as z
+		on a.internal_member_id = z.id_apcd
+		where a.icd_procedure_code not in ('-1','-2')
+		--exclude denined/orphaned claims
+		and (c.denied_header_flag = 'N' and c.orphaned_header_flag = 'N')
+		--exclude members with no WA residency OR no elig data
+		and (y.id_apcd is null and z.id_apcd is null)
+	)
+	--Union and insert into table shell
+	insert into stg_claims.apcd_claim_procedure
+	select *, getdate() as last_run from line_procedure
+	union select *, getdate() as last_run from icd_procedure;",
     .con = dw_inthealth))
 }
 
@@ -35,7 +104,7 @@ load_stage.apcd_claim_procedure_f <- function() {
 #### Table-level QA script ####
 qa_stage.apcd_claim_procedure_f <- function() {
   
-  #all members should be in elig_demo and elig_timevar tables
+  #all members should be in elig_demo and elig_month tables
   res1 <- dbGetQuery(conn = dw_inthealth, glue_sql(
     "select 'stg_claims.stage_apcd_claim_procedure' as 'table', '# members not in elig_demo, expect 0' as qa_type,
     count(distinct a.id_apcd) as qa
@@ -46,10 +115,10 @@ qa_stage.apcd_claim_procedure_f <- function() {
     .con = dw_inthealth))
   
   res2 <- dbGetQuery(conn = dw_inthealth, glue_sql(
-    "select 'stg_claims.stage_apcd_claim_procedure' as 'table', '# members not in elig_timevar, expect 0' as qa_type,
+    "select 'stg_claims.stage_apcd_claim_procedure' as 'table', '# members not in elig_month, expect 0' as qa_type,
     count(distinct a.id_apcd) as qa
     from stg_claims.stage_apcd_claim_procedure as a
-    left join stg_claims.stage_apcd_elig_timevar as b
+    left join stg_claims.stage_apcd_elig_month as b
     on a.id_apcd = b.id_apcd
     where b.id_apcd is null;",
     .con = dw_inthealth))
