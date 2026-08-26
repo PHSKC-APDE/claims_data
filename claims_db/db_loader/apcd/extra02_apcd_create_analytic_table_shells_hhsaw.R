2c1daa1d3a393b8eff9957d52be330a4db72819d
Eli Kern <eli.kern@kingcounty.gov>
2026-08-26 15:44:30 -0700

    Adding use of apde.etl package

--- claims_db/db_loader/apcd/extra02_apcd_create_analytic_table_shells_hhsaw.R
+++ claims_db/db_loader/apcd/extra02_apcd_create_analytic_table_shells_hhsaw.R
@@ -2,12 +2,14 @@
 #
 # Eli Kern, PHSKC (APDE)
 # 2024-06
+#
+# 2024-08-26 update: Modifying to use apde.etl package
 
 #### Set up global parameter and call in libraries ####
 options(max.print = 350, tibble.print_max = 50, warning.length = 8170, scipen = 999)
 
 library(pacman)
-pacman::p_load(tidyverse, lubridate, odbc, glue)
+pacman::p_load(tidyverse, lubridate, odbc, glue, apde.etl)
 
 #### SET UP FUNCTIONS ####
 devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/db_loader/mcaid/create_db_connection.R")
@@ -21,10 +23,10 @@ db_claims <- create_db_connection("hhsaw", interactive = interactive_auth, prod 
 
 ## Create and index table shells if table does not exist
 # Remember to reuse YAML config files used for Synapse, need to manually pass schema and table name to create_table function
-#change to 'T' when it's an existing table
+#change overwrite to 'T' when it's an existing table
 ## [FILL IN BLANKS BEFORE USING]
 
-create_table(
+apde.etl::create_table(
   conn = db_claims, 
   config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/phclaims/stage/tables/load_stage.BLANK.yaml",
   overall = T, ind_yr = F, overwrite = F, server = "hhsaw", to_schema = "claims", to_table = "BLANK")
@@ -33,32 +35,12 @@ dbExecute(
   conn = db_claims,
   glue_sql("create clustered columnstore index idx_ccs_BLANK on claims.BLANK;"))
 
-##EXAMPLES: 
-create_table(
-  conn = db_claims, 
-  config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/refs/heads/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_header.yaml",
-  overall = T, ind_yr = F, overwrite = T, server = "hhsaw", to_schema = "claims", to_table = "final_apcd_claim_header")
-
-dbExecute(
-  conn = db_claims,
-  glue_sql("create clustered columnstore index idx_ccs_BLANK on claims.final_apcd_claim_header;"))
-
-
-create_table(
-  conn = db_claims, 
-  config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/refs/heads/main/claims_db/phclaims/stage/tables/load_stage.apcd_elig_plr_2024.yaml",
-  overall = T, ind_yr = F, overwrite = T, server = "hhsaw", to_schema = "claims", to_table = "final_apcd_elig_plr_2024")
-
-dbExecute(
-  conn = db_claims,
-  glue_sql("create clustered columnstore index idx_ccs_BLANK on claims.final_apcd_elig_plr_2024;"))
-
-
-create_table(
-  conn = db_claims, 
-  config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/refs/heads/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_procedure.yaml",
-  overall = T, ind_yr = F, overwrite = T, server = "hhsaw", to_schema = "claims", to_table = "final_apcd_claim_procedure")
-
-dbExecute(
-  conn = db_claims,
-  glue_sql("create clustered columnstore index idx_ccs_BLANK on claims.final_apcd_claim_procedure;"))
+##EXAMPLE: 
+# apde.etl::create_table(
+#   conn = db_claims, 
+#   config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/refs/heads/main/claims_db/phclaims/stage/tables/load_stage.apcd_elig_plr_2024.yaml",
+#   overall = T, ind_yr = F, overwrite = T, server = "hhsaw", to_schema = "claims", to_table = "final_apcd_elig_plr_2024")
+# 
+# dbExecute(
+#   conn = db_claims,
+#   glue_sql("create clustered columnstore index idx_ccs_BLANK on claims.final_apcd_elig_plr_2024;"))
