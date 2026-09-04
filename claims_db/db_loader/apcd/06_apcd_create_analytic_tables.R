ca106ad1490165452eec296dc93a57e394e838ad
Eli Kern <eli.kern@kingcounty.gov>
2026-09-04 12:43:18 -0700

    Changing script number in file name

--- claims_db/db_loader/apcd/06_apcd_create_analytic_tables.R
+++ claims_db/db_loader/apcd/06_apcd_create_analytic_tables.R
@@ -341,9 +341,9 @@ dw_inthealth <- create_db_connection("inthealth", interactive = interactive_auth
 devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_icdcm_header.R")
 
 ### B) Create table
-create_table(conn = dw_inthealth, config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_icdcm_header.yaml",
-             overall = T, ind_yr = F, overwrite = T, server = "hhsaw",
-             with = "DISTRIBUTION = HASH(claim_header_id)")
+# create_table(conn = dw_inthealth, config_url = "https://raw.githubusercontent.com/PHSKC-APDE/claims_data/main/claims_db/phclaims/stage/tables/load_stage.apcd_claim_icdcm_header.yaml",
+#              overall = T, ind_yr = F, overwrite = T, server = "hhsaw",
+#              with = "DISTRIBUTION = HASH(claim_header_id)")
 
 ### C) Load tables
 system.time(load_stage.apcd_claim_icdcm_header_f())
