8b74d92e6031211039f5c48bd137a9a29b3ebe1f
Eli Kern <eli.kern@kingcounty.gov>
2026-09-14 08:59:25 -0700

    Revert "Trying to add DISTINCT back"
    This reverts commit eafba742fae9bdba6eca7a5ce09b0a9ddade56de.

--- claims_db/phclaims/stage/tables/load_stage.apcd_claim_icdcm_header.R
+++ claims_db/phclaims/stage/tables/load_stage.apcd_claim_icdcm_header.R
@@ -19,6 +19,7 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
     "
 	--STEP 0: Cleanup old temp tables
 	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_d1_raw', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_d1_raw;
+	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_d1_dedup', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_d1_dedup;
 	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_header_raw', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_header_raw;
 	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_norm', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_norm;
 	
@@ -27,10 +28,10 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
 	WITH
 	(
 	DISTRIBUTION = HASH(claim_header_id),
-	HEAP
+	CLUSTERED COLUMNSTORE INDEX
 	)
 	AS
-	SELECT DISTINCT
+	SELECT
 	a.internal_member_id AS id_apcd,
 	b.medical_claim_header_id AS claim_header_id,
 	c.first_service_dt AS first_service_date,
@@ -67,7 +68,25 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
 	--exclude members with no WA residency OR no elig data
 	AND y.id_apcd IS NULL;
 
-	--STEP 2: Extract primary diagnsosis codes from medical claim header table, applying exclusions
+	--STEP 2: Deduplicate line-level diagnosis codes (faster than using DISTINCT in first step)
+	CREATE TABLE stg_claims.tmp_apcd_icdcm_d1_dedup
+	WITH
+	(
+	DISTRIBUTION = HASH(claim_header_id),
+	CLUSTERED COLUMNSTORE INDEX
+	)
+	AS
+	SELECT *
+	FROM
+	(
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY id_apcd, claim_header_id, icdcm_raw ORDER BY id_apcd) AS rn
+	FROM stg_claims.tmp_apcd_icdcm_d1_raw
+	) x
+	WHERE rn = 1;
+	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_d1_raw', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_d1_raw;
+
+	--STEP 3: Extract primary diagnsosis codes from medical claim header table, applying exclusions
 	--Note that ICD-CM version in this table does not need correcting (all first digit alpha codes > 2015-10-01 are V and E codes)
 	CREATE TABLE stg_claims.tmp_apcd_icdcm_header_raw
 	WITH
@@ -95,7 +114,7 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
 	--exclude members with no WA residency OR no elig data
 	AND y.id_apcd IS NULL;
 
-	--STEP 3: Normalize ICD-CM codes and union tables
+	--STEP 4: Normalize ICD-CM codes and union tables
 	CREATE TABLE stg_claims.tmp_apcd_icdcm_norm
 	WITH
 	(
@@ -117,7 +136,7 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
 	END AS icdcm_norm,
 	icdcm_version,
 	icdcm_number
-	FROM stg_claims.tmp_apcd_icdcm_d1_raw
+	FROM stg_claims.tmp_apcd_icdcm_d1_dedup
 	UNION ALL
 	SELECT
 	id_apcd,
@@ -134,11 +153,11 @@ load_stage.apcd_claim_icdcm_header_f <- function() {
 	icdcm_version,
 	icdcm_number
 	FROM stg_claims.tmp_apcd_icdcm_header_raw;
-	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_d1_raw', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_d1_raw;
+	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_d1_dedup', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_d1_dedup;
 	IF OBJECT_ID('stg_claims.tmp_apcd_icdcm_header_raw', 'U') IS NOT NULL DROP TABLE stg_claims.tmp_apcd_icdcm_header_raw;
 	
 
-	--STEP 4: Create icdcm_hash column and create final table (CTA is faster than INSERT INTO in Synapse)
+	--STEP 5: Create icdcm_hash column and create final table (CTA is faster than INSERT INTO in Synapse)
 	IF OBJECT_ID('stg_claims.stage_apcd_claim_icdcm_header', 'U') IS NOT NULL DROP TABLE stg_claims.stage_apcd_claim_icdcm_header;
 	CREATE TABLE stg_claims.stage_apcd_claim_icdcm_header
 	WITH
